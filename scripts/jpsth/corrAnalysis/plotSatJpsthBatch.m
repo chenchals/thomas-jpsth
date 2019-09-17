3873cb8919e231e43dc459734fe82108c2294367
Chenchal Subraveti <chenchals@gmail.com>
2019-09-17 17:47:17 -0500

    updated to regenerate all pdfs

--- scripts/jpsth/corrAnalysis/plotSatJpsthBatch.m
+++ scripts/jpsth/corrAnalysis/plotSatJpsthBatch.m
@@ -1,18 +1,25 @@
 
 jpsthDataBaseDir = 'dataProcessed/analysis/JPSTH';
 
-pairDirs = dir(fullfile(jpsthDataBaseDir,'jpsth_*'));
-pairDirs = strcat({pairDirs.folder}',filesep,{pairDirs.name}');
+pairDirs = {
+    'jpsth_SEF-FEF/mat'
+    'jpsth_SEF-SC/mat'
+    'jpsth_FEF-SC/mat'
+    'jpsth_SEF-SEF/mat'
+    'jpsth_FEF-FEF/mat'
+    'jpsth_SC-SC/mat'
+    };
+
 tic
 saveFigFlag = 1;
 for pd = 1:numel(pairDirs)
     pairDir = pairDirs{pd};
-    pdfOutputDir = fullfile(pairDir,'pdf');
+    pdfOutputDir = fullfile(pairDir,'pdf2');
     if ~exist(pdfOutputDir,'dir')
       mkdir(pdfOutputDir);
     end
     
-    jpsthPairFiles = dir(fullfile(pairDir,'mat/JPSTH-PAIR_*.mat'));
+    jpsthPairFiles = dir(fullfile(pairDir,'JPSTH-PAIR_*.mat'));
     jpsthPairFiles = strcat({jpsthPairFiles.folder}',filesep,{jpsthPairFiles.name}');
     % cant do parfor as plot gets clipped
     for pf = 1:numel(jpsthPairFiles)
