47231072581832310c9a0ca6b9ccd139dd486a1b
chenchals <chenchals@gmail.com>
2019-09-18 13:52:08 -0500

    updated with parfor for processing...

--- scripts/jpsth/corrAnalysis/pstchXcorrExtract.m
+++ scripts/jpsth/corrAnalysis/pstchXcorrExtract.m
@@ -48,9 +48,12 @@ for ii = 1: 1 %nDirs
     jpsthPairFiles = dir(fullfile(pairDir,'JPSTH-PAIR_*.mat'));
     jpsthPairFiles = strcat({jpsthPairFiles.folder}',filesep,{jpsthPairFiles.name}');
     cellPairInfo = table();
+    
+    out = cell(4,1);%numel(jpsthPairFiles),1);
     outData = table();
- 
-    for jj = 1:4 %numel(jpsthPairFiles)
+    parfor jj = 1:4 %numel(jpsthPairFiles)
+        out{jj}.outData = table();
+        oD = table();
         pairFile = jpsthPairFiles{jj};
         fprintf('Processing file %s\n',pairFile)
         temp = load(pairFile,'cellPairInfo');
@@ -59,24 +62,32 @@ for ii = 1: 1 %nDirs
             condition = conditions{kk};
             data = load(pairFile,condition);
             data = data.(condition);
-            nRows = size(outData,1) + 1;
+            nRows = size(out{jj}.outData,1) + 1;
+            nR = size(oD,1) + 1; 
             for ll = 1:numel(winChoices)
-                outData.cellPairId{nRows} = temp.cellPairInfo.Pair_UID;
-                outData.condition{nRows} = condition;
+                oD.cellPairId{nRows} = temp.cellPairInfo.Pair_UID;
+                oD.condition{nRows} = condition;
+                out{jj}.outData.cellPairId{nRows} = temp.cellPairInfo.Pair_UID;
+                out{jj}.outData.condition{nRows} = condition;
                 rowName = winChoices(ll).name;
                 winLims = winChoices(ll).winLims;
-                outData.([rowName '_winLims']){nRows} = winLims;
+                oD.([rowName '_winLims']){nRows} = winLims;
+                out{jj}.outData.([rowName '_winLims']){nRows} = winLims;
                 times = data{rowName,'xPsthBins'}{1}(:);
                 startIdx = find(times==winLims(1));
-                endIdx = find(times==winLims(2));
-                
-                outData.([rowName '_coins']){nRows} = sum(data{rowName,'coincidenceHist'}{1}(startIdx:endIdx,2));
+                endIdx = find(times==winLims(2));                
+                oD.([rowName '_coins']){nRows} = sum(data{rowName,'coincidenceHist'}{1}(startIdx:endIdx,2));
+                out{jj}.outData.([rowName '_coins']){nRows} = sum(data{rowName,'coincidenceHist'}{1}(startIdx:endIdx,2));
             end
-        end
-        
+        end   
+        out1 = [out1;oD];
     end
-    
-    
+    %out = [out{:}];
+    tempData = table();
+    parfor mm = 1:size(out,1)
+        t = out{mm};
+        tempData = [tempData;t.outData];
+    end
 end
 
 
