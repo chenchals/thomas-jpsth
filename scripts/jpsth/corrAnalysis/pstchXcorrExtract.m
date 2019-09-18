c9488b71b9ba62d1161369ff3ad723fdbe9d3c48
chenchals <chenchals@gmail.com>
2019-09-18 15:04:18 -0500

    updated for extracting values fro JPSTH variables for statistics...

--- scripts/jpsth/corrAnalysis/pstchXcorrExtract.m
+++ scripts/jpsth/corrAnalysis/pstchXcorrExtract.m
@@ -4,6 +4,7 @@
 
 %%
 jpsthDataBaseDir = 'dataProcessed/analysis/JPSTH';
+outputDir = 'dataProcessed/analysis/JPSTH-extract1';
 
 pairDirs = {
     'jpsth_SEF-FEF/mat'
@@ -13,10 +14,9 @@ pairDirs = {
     'jpsth_FEF-FEF/mat'
     'jpsth_SC-SC/mat'
     };
+outputFiles = cellfun(@(x) fileparts(x),pairDirs,'UniformOutput',false);
 
 pairDirs = strcat(jpsthDataBaseDir,filesep,pairDirs);
-
-outputDir = 'dataProcessed/analysis/JPSTH-extract1';
 if ~exist(outputDir,'dir')
     mkdir(outputDir);
 end
@@ -40,19 +40,21 @@ winChoices(2).winLims = [50 200];
 winChoices(3).name = 'PostSaccade';
 winChoices(3).winLims = [50 300];
 
-for ii = 1: 1 %nDirs
+xCorrWin = [-50 50];
+
+% see doc pctRunOnAll
+pctRunOnAll warning off;
+for ii = 1:nDirs
+    tic
     nRows = 0;
     pairDir = pairDirs{ii}; 
-    [~,outputFile] = fileparts(pairDir);
-    outputFile = fullfile(outputDir,[outputFile '.mat']);
+    fprintf('Doing pairDir %s\n',pairDir)
+    outputFile = fullfile(outputDir,[lower(outputFiles{ii}) '.mat']);
     jpsthPairFiles = dir(fullfile(pairDir,'JPSTH-PAIR_*.mat'));
     jpsthPairFiles = strcat({jpsthPairFiles.folder}',filesep,{jpsthPairFiles.name}');
     cellPairInfo = table();
-    
-    out = cell(4,1);%numel(jpsthPairFiles),1);
     outData = table();
-    parfor jj = 1:4 %numel(jpsthPairFiles)
-        out{jj}.outData = table();
+    parfor jj = 1:numel(jpsthPairFiles)
         oD = table();
         pairFile = jpsthPairFiles{jj};
         fprintf('Processing file %s\n',pairFile)
@@ -62,32 +64,33 @@ for ii = 1: 1 %nDirs
             condition = conditions{kk};
             data = load(pairFile,condition);
             data = data.(condition);
-            nRows = size(out{jj}.outData,1) + 1;
-            nR = size(oD,1) + 1; 
+            nRows = size(oD,1) + 1; 
             for ll = 1:numel(winChoices)
                 oD.cellPairId{nRows} = temp.cellPairInfo.Pair_UID;
                 oD.condition{nRows} = condition;
-                out{jj}.outData.cellPairId{nRows} = temp.cellPairInfo.Pair_UID;
-                out{jj}.outData.condition{nRows} = condition;
                 rowName = winChoices(ll).name;
                 winLims = winChoices(ll).winLims;
                 oD.([rowName '_winLims']){nRows} = winLims;
-                out{jj}.outData.([rowName '_winLims']){nRows} = winLims;
+                % 
                 times = data{rowName,'xPsthBins'}{1}(:);
                 startIdx = find(times==winLims(1));
                 endIdx = find(times==winLims(2));                
                 oD.([rowName '_coins']){nRows} = sum(data{rowName,'coincidenceHist'}{1}(startIdx:endIdx,2));
-                out{jj}.outData.([rowName '_coins']){nRows} = sum(data{rowName,'coincidenceHist'}{1}(startIdx:endIdx,2));
+                oD.([rowName '_xSpikeCounts']){nRows} = sum(sum(data{rowName,'xSpikeCounts'}{1}(:,startIdx:endIdx)));
+                oD.([rowName '_ySpikeCounts']){nRows} = sum(sum(data{rowName,'ySpikeCounts'}{1}(:,startIdx:endIdx)));
+                %
+                times = data{rowName,'xCorrHist'}{1}(:,1);               
+                oD.([rowName '_xCorrWinLims']){nRows} = xCorrWin;
+                oD.([rowName '_xCorrHist']){nRows} = sum(data{rowName,'xCorrHist'}{1}...
+                                (times>=xCorrWin(1) & times<=xCorrWin(2),2)); %#ok<*PFBNS>
             end
-        end   
-        out1 = [out1;oD];
-    end
-    %out = [out{:}];
-    tempData = table();
-    parfor mm = 1:size(out,1)
-        t = out{mm};
-        tempData = [tempData;t.outData];
-    end
+        end
+        outData = [outData;oD];
+    end % end parfor 
+    tempData.cellPairInfo = cellPairInfo;
+    tempData.jpsthExtract = outData;
+    save(outputFile,'-v7.3','-struct','tempData');
+    fprintf('Done doing pairDir %s, took %d s\n',pairDir, round(toc,2))
 end
 
 
