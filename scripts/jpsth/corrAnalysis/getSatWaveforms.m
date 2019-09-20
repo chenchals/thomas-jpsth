146e7f779c3968dd1eab92a4d69ee193f33be606
chenchals <chenchals@gmail.com>
2019-09-20 09:47:53 -0500

    Added readPLX to extract waveforms

--- scripts/jpsth/corrAnalysis/getSatWaveforms.m
+++ scripts/jpsth/corrAnalysis/getSatWaveforms.m
@@ -21,3 +21,54 @@ eTtmingPairs = sortrows([A;B],'unitNum');
 
 clearvars A B jpsthPairs
 
+
+%% Error Choice Pair 0189 - 13a, 13b
+pairInfo = eChoicePairs(strcmpi(eChoicePairs.Pair_UID,'PAIR_0189'),:);
+matFile = pairInfo.matDatafile{1}; % maybe duplicates are present
+% need ot translate or do manually for all sessions
+plxFile = 'Z:/data/Euler/SAT/Plexon/Sorted/E20130911001-RH.plx';
+plxData = readPLXFileC(plxFile,'all');
+chData = plxData.SpikeChannels([plxData.SpikeChannels.Channel]==13);
+spikeData.matFile = matFile;
+spikeData.plexonFile = 'data/Euler/SAT/Plexon/Sorted/E20130911001-RH.plx';
+spikeData.pairID = pairInfo.Pair_UID;
+spikeData.channel = 13;
+spikeData.clustNo = unique(chData.Units);
+spikeData.spkCounts = arrayfun(@(x) sum(chData.Units==x),spikeData.clustNo,'UniformOutput',false);
+spikeData.spkTimes = arrayfun(@(x) double(chData.Timestamps(chData.Units==x)),spikeData.clustNo,'UniformOutput',false);
+spikeData.waves = arrayfun(@(x) double(chData.Waves(:,chData.Units==x)),spikeData.clustNo,'UniformOutput',false);
+spikeData.mean = cellfun(@(x) mean(x,2),spikeData.waves,'UniformOutput',false);
+spikeData.std = cellfun(@(x) std(x,[],2),spikeData.waves,'UniformOutput',false);
+plotWfMeanVar(spikeData);
+
+
+%%
+function plotWf(wavformMat)
+% Each column is a waveform
+  nPoints = size(wavformMat,1);
+  nWaves = size(wavformMat,2);
+  temp = [wavformMat;nan(1,nWaves)];
+  temp = temp(:);
+  x = repmat([1:nPoints NaN]',1,nWaves);
+  x = x(:);
+  plot(x,y)
+end
+
+function plotWfMeanVar(spikeData)
+    colors = {'b','r','c'};
+    for ii = 1:numel(spikeData.mean)
+        spkCounts = spikeData.spkCounts{ii};
+        clustNum = spikeData.clustNo(ii);
+        clustLetter = char(clustNum+97);
+        labels{2*ii-1} = '';
+        labels{2*ii} = sprintf('Unit %d%s n=%d',spikeData.channel,clustLetter,spkCounts);
+        y = (spikeData.mean{ii})';
+        x = (1:32);
+        yStd = (spikeData.std{ii})';
+        fill([x fliplr(x)], [y+yStd fliplr(y-yStd)],colors{ii},'FaceAlpha',0.5,'LineStyle','none')
+        hold on
+        plot(x,y,colors{ii},'LineWidth',1.5)
+    end
+    legend(labels,'Location','northwest','Box','off')
+    hold off
+end
