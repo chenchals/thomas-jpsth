00bdc98b8ed80dbd9c91e4766d73329432b8627d
Chenchal Subraveti <chenchals@gmail.com>
2019-09-23 18:38:53 -0500

    updated to use waves directory...

--- scripts/jpsth/corrAnalysis/exploreWavforms.m
+++ scripts/jpsth/corrAnalysis/exploreWavforms.m
@@ -1,34 +1,21 @@
 
-% matfile 'teba/data' dir mapping location
-matDataDir = 'T:'; 
-% load all waveforms for SAT
-%allUnits = load('E:/waves_SAT_DaEu1.mat');
-%allUnits = load('dataProcessed/dataset/waves_SAT_DaEu1.mat');
-
-% list of units:
-unitList = fieldnames(allUnits);
-% remove all names that do not start with Unit_
-% cheat...
-unitList = unitList(1:end-2);
-
+waveDataDir = 'dataProcessed/dataset/waves';
 % load all SAT spikes
-allSpks = load('dataProcessed\dataset\spikes_SAT.mat');
+allSpks = load('dataProcessed/dataset/spikes_SAT.mat');
 allSpks = allSpks.spikes;
 
 % load ninfo for getting the unit number / unit name...
-ninfo = load('dataProcessed\dataset\ninfo_nstats_SAT.mat','ninfo');
+ninfo = load('dataProcessed/dataset/ninfo_nstats_SAT.mat','ninfo');
 ninfo = struct2table(ninfo.ninfo);
 % choose unit no...
 unitNo = 41;
-wavUnitTbl = allUnits.(num2str(unitNo,'Unit_%03d'));
+
+unitName = num2str(unitNo,'Unit_%03d');
+wavUnitTbl = load(fullfile(waveDataDir,[unitName '.mat']));
+wavUnitTbl = wavUnitTbl.(unitName);
 spkUnitTrls = allSpks(ninfo.unitNum==unitNo).SAT';
 % verify no of trials is correct for wav and spk
 assert(size(wavUnitTbl.wavSearch{1},1)==numel(spkUnitTrls),'Error different no. of trials')
-
-% get the TrialStart_ time from the mat file...
-matData = load(fullfile(matDataDir,wavUnitTbl.matFile{1}),'TrialStart_');
-
-
 
 % for sel. trials get 'matching' waveforms for spks
 selTrls = [10 12 128];
@@ -39,15 +26,12 @@ for tt = 1:numel(selTrls)
    % all waveforms for spikes in trial
    trlWf = wavUnitTbl.wavSearch{1}{trlNo};
    % timestamps got from waveforms
-   trlWfTs =  round((wavUnitTbl.wavSearchTs{1}{trlNo}-trlStartTs));
+   trlWfTs =  wavUnitTbl.wavSearchTs{1}{trlNo};
    % timestamps from unit in mat file / ninfo
    trlUnitTs = spkUnitTrls{trlNo}';
    % find indices of spikes that are closest match
    spkIdx = arrayfun(@(uTs) find(abs(trlWfTs - uTs)<=diffMax,1),trlUnitTs);
    plotWaveforms(trlWf(spkIdx,:))
-   
-   
-   plot(1:32,trlWf(1:end,:))
 end
 
 
--- scripts/jpsth/corrAnalysis/plotWaveforms.m
+++ scripts/jpsth/corrAnalysis/plotWaveforms.m
@@ -1,7 +1,7 @@
 
-function plotWaveforms(wavformMat)
+function plotWaveforms(wavformMat,varargin)
   % Each column should be a waveform
-  pColor = 'm';
+  pColor = 'b';
   wavformMat = wavformMat';
   nPoints = size(wavformMat,1);
   nWaves = size(wavformMat,2);
@@ -10,16 +10,16 @@ function plotWaveforms(wavformMat)
   % plot mean and std
   meanWf = mean(wavformMat,2)';
   stdWf = std(wavformMat,[],2)';
-  p = plot(x,meanWf,'LineWidth',1.5,'Color',pColor);
+  p = plot(x,meanWf,'LineWidth',2.5,'Color',pColor);
   c = p.Color;
   hold on
-  fill([x fliplr(x)], [meanWf+stdWf fliplr(meanWf-stdWf)],c,'FaceAlpha',0.5,'LineStyle','none')
+  fill([x fliplr(x)], [meanWf+stdWf fliplr(meanWf-stdWf)],c,'FaceAlpha',0.4,'LineStyle','none')
   % individual waveforms
   temp = [wavformMat;nan(1,nWaves)];
   temp = temp(:);
   x = repmat([1:nPoints NaN]',1,nWaves);
   x = x(:);
-  ph = plot(x,temp);
-  ph.Color = [c(1:3) 0.3];
+  ph = plot(x,temp,'k','LineWidth',0.1);
+  ph.Color(4) = 0.2;
   hold off
 end
