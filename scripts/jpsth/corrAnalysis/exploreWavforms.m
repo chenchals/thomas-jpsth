2ba5f04ca148a01c1d9548ddae8ef88df54f04aa
chenchals <chenchals@gmail.com>
2019-09-25 11:34:27 -0500

    update to do the aggregation of waveforms after alignemtn for selected trials... in progress

--- scripts/jpsth/corrAnalysis/exploreWavforms.m
+++ scripts/jpsth/corrAnalysis/exploreWavforms.m
@@ -15,30 +15,66 @@ sessionEventTimes = sessionEventTimes.TrialEventTimesDB;
 % get Conditions for sessions (Accurate, Fast, ...)
 sessionTrialTypes = load('dataProcessed/dataset/TrialTypesDB.mat');
 sessionTrialTypes = sessionTrialTypes.TrialTypesDB;
-
+% inline function for aligning, windowing TS of trials
+fx_alignTs = @(tsByTrl,alinTimeByTrl) arrayfun(@(idx) tsByTrl{idx} - alinTimeByTrl(idx),(1:numel(alinTimeByTrl))','UniformOutput',false);
+fx_alindTsToWin = @(alindTsByTrl, tsWin) cellfun(@(x) x(x>=tsWin(1) & x<=tsWin(2)),alindTsByTrl,'UniformOutput',false);
+% find matching indices for 2 arrrays of aligned ts
+% max abs. diff is 1 ms
+diffMax = 1; % 1 ms different
+fx_matchedSpkIdx = @(alindUTs,alindWfTs) arrayfun(@(uTs) find(abs(alindWfTs - uTs)<=diffMax,1),alindUTs,'UniformOutput',false);
 
 
 
 % choose unit no...
 unitNo = 41;
+% trialType
+selTrialType = 'FastErrorChoice';
+% note CueOn == TrialStart
+alignedEvent = 'CueOn';
+alignWin = [-200 400];
 
+% load unit
 unitName = num2str(unitNo,'Unit_%03d');
 wavUnitTbl = load(fullfile(waveDataDir,[unitName '.mat']));
 wavUnitTbl = wavUnitTbl.(unitName);
 session = wavUnitTbl.session{1};
 
-% All trials for that unit/session
+% Event times
+evntTimes = sessionEventTimes(strcmp(sessionEventTimes.session,session),:);
+alignTime = evntTimes.CueOn{1};
+if ~strcmp(alignedEvent,'CueOn')
+    alignTime = alignTime + evntTimes.(alignedEvent){1}(:);
+end
+
+% load trials for that unit/session
 spkUnitTrls = allSpks(ninfo.unitNum==unitNo).SAT';
 % verify no of trials is correct for wav and spk
 assert(size(wavUnitTbl.wavSearch{1},1)==numel(spkUnitTrls),'Error different no. of trials')
 
-% Trial types for the session
+% Selected Trial types for the session
 trialTypes = sessionTrialTypes(strcmp(sessionTrialTypes.session,session),:);
-% Event times
-evntTimes = sessionEventTimes(strcmp(sessionEventTimes.session,session),:);
-% for sel. trials get 'matching' waveforms for spks
-selTrls = [10 12 128];
-diffMax = 1; % 1 ms different
+% trial selection, and alignment
+selTrls = find(trialTypes.(selTrialType){1});
+% align and select ts in window after alignment
+alindUnitTs = fx_alignTs(spkUnitTrls(selTrls),alignTime(selTrls));
+alindUnitTsWin = fx_alindTsToWin(alindUnitTs,alignWin);
+% align ts from waveform data
+alindWfTs = fx_alignTs(wavUnitTbl.wavSearchTs{1}(selTrls),alignTime(selTrls));
+alindWfTsWin = fx_alindTsToWin(alindWfTs,alignWin);
+% find matching indices for extracting waveforms
+mathedSpkIdx = cellfun(@(aUts,aWts) fx_matchedSpkIdx(aUts,aWts),alindUnitTsWin,alindWfTsWin,'UniformOutput',false);
+
+   % all waveforms for spikes in trial
+   trlWf = wavUnitTbl.wavSearch{1}{trlNo};
+   % timestamps got from waveforms
+
+
+
+
+
+
+
+
 for tt = 1:numel(selTrls)
    trlNo = selTrls(tt);
    %trlStartTs = matData.TrialStart_(trlNo,1);
@@ -50,13 +86,13 @@ for tt = 1:numel(selTrls)
    trlUnitTs = spkUnitTrls{trlNo}';
    
    % Align and subset the spike times you want
-   alignedUnitTs = trlUnitTs;
+   alindUnitTs = trlUnitTs;
    alignedWfTs = trlWfTs;
    
    % find indices of spikes that are closest match
-   spkIdx = arrayfun(@(uTs) find(abs(alignedWfTs - uTs)<=diffMax,1),alignedUnitTs);
-   plotWaveforms(trlWf(spkIdx,:))
-   pause
+   wfs{tt,1} = trlWf(fx_matchedSpkIdx(alindUnitTs,alignedWfTs),:);
+   %plotWaveforms(trlWf(spkIdx,:))
+   %pause
 end
 
 
