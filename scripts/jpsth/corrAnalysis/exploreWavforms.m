e62bd08bf27fc4b176ae8d664d89740f9b9b2121
chenchals <chenchals@gmail.com>
2019-09-25 14:46:20 -0500

    updated removed junk

--- scripts/jpsth/corrAnalysis/exploreWavforms.m
+++ scripts/jpsth/corrAnalysis/exploreWavforms.m
@@ -15,7 +15,8 @@ sessionEventTimes = sessionEventTimes.TrialEventTimesDB;
 % get Conditions for sessions (Accurate, Fast, ...)
 sessionTrialTypes = load('dataProcessed/dataset/TrialTypesDB.mat');
 sessionTrialTypes = sessionTrialTypes.TrialTypesDB;
-% inline function for aligning, windowing TS of trials
+
+%% inline function for aligning, windowing TS of trials
 fx_alignTs = @(tsByTrl,alinTimeByTrl) arrayfun(@(idx) tsByTrl{idx} - alinTimeByTrl(idx),(1:numel(alinTimeByTrl))','UniformOutput',false);
 fx_alindTsToWin = @(alindTsByTrl, tsWin) cellfun(@(x) x(x>=tsWin(1) & x<=tsWin(2)),alindTsByTrl,'UniformOutput',false);
 % find matching indices for 2 arrrays of aligned ts
@@ -24,7 +25,7 @@ diffMax = 1; % 1 ms different
 fx_matchedSpkIdx = @(alindUTs,alindWfTs) arrayfun(@(uTs) find(abs(alindWfTs - uTs)<=diffMax,1),alindUTs,'UniformOutput',false);
 
 
-
+%% for a given unit, condition, alignment, align-window plot selected waveforms
 % choose unit no...
 unitNo = 41;
 % trialType
@@ -68,50 +69,6 @@ allWfBytrl = wavUnitTbl.wavSearch{1}(selTrls);
 alindWfWin = cellfun(@(wfByTrl,idxByTrl) wfByTrl(cell2mat(idxByTrl'),:),allWfBytrl,spkIdxByTrl,'UniformOutput',false);
 
 plotWaveforms(cell2mat(alindWfWin));
-
-
-
-
-
-
-
-for tt = 1:numel(selTrls)
-   trlNo = selTrls(tt);
-   %trlStartTs = matData.TrialStart_(trlNo,1);
-   % all waveforms for spikes in trial
-   trlWf = wavUnitTbl.wavSearch{1}{trlNo};
-   % timestamps got from waveforms
-   trlWfTs =  wavUnitTbl.wavSearchTs{1}{trlNo};
-   % timestamps from unit in mat file / ninfo
-   trlUnitTs = spkUnitTrls{trlNo}';
-   
-   % Align and subset the spike times you want
-   alindUnitTs = trlUnitTs;
-   alignedWfTs = trlWfTs;
-   
-   % find indices of spikes that are closest match
-   wfs{tt,1} = trlWf(fx_matchedSpkIdx(alindUnitTs,alignedWfTs),:);
-   %plotWaveforms(trlWf(spkIdx,:))
-   %pause
-end
-
-
-% Plot aggregated waveform for all selected trials and spikes
-alignEvent = 'CueOn';
-
-selTrls = find(trialTypes.FastErrorChoice{1});
-alignedUnitTs = arrayfun(@(idx) spkUnitTrls{selTrls(idx)} -  
-
-
-
-
-
-allWf = wavUnitTbl.wavSearch{1};
-allWfTs = wavUnitTbl.wavSearchTs{1};
-allUnitTs = [spkUnitTrls(selTrls)];
-allWf4SelTrls = arrayfun(@(tn) {tn},selTrls);
-allWfTs4SelTrls = arrayfun(@(tn) wavUnitTbl.wavSearch{1}{tn},selTrls);
-
 
 
 
