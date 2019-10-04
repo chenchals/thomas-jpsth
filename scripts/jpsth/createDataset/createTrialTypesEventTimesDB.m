7a7d128c1359b6c79a16a9cf90676809b4521e81
chenchals <chenchals@gmail.com>
2019-10-03 21:16:30 -0500

    Updated the filenamesfor output after verifying second sacc / reward

--- scripts/jpsth/createDataset/createTrialTypesEventTimesDB.m
+++ scripts/jpsth/createDataset/createTrialTypesEventTimesDB.m
@@ -5,8 +5,8 @@ datasetDir = 'dataProcessed/dataset';
 jpsthPairsDBFile = fullfile(datasetDir,'JPSTH_PAIRS_CellInfoDB.mat');
 binfoMovesFile = fullfile(datasetDir,'binfo_moves_SAT.mat');
 % Output dataset files
-trialTypesFile = fullfile(datasetDir,'out(s).TrialTypesDB_2.mat');
-TrialEventTimesFile = fullfile(datasetDir,'out(s).TrialEventTimesDB_2.mat');
+trialTypesFile = fullfile(datasetDir,'TrialTypesDB_2.mat');
+TrialEventTimesFile = fullfile(datasetDir,'TrialEventTimesDB_2.mat');
 
 %% Process for trial event times and trial types
 
