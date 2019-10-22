04eb7299e198f9c0ebbb3b6566eb2c433145dcbf
Chenchal Subraveti <chenchals@gmail.com>
2019-10-22 17:12:28 -0500

    updated plot of spike width distributions for SEF, FEF, and SC

--- scripts/jpsth/corrAnalysis/spikeWidthDistrib.m
+++ scripts/jpsth/corrAnalysis/spikeWidthDistrib.m
@@ -45,8 +45,57 @@ scWidths = cell2mat(wavWidthsTbl.wavWidthSearchMs(strcmp(wavWidthsTbl.area,'SC')
 minWidth = min([sefWidths;fefWidths;scWidths]);
 maxWidth = max([sefWidths;fefWidths;scWidths]);
 
+binWidth = 0.025;
+[sefC,be] = histcounts(sefWidths,'BinLimits',[0 1],'BinWidth',binWidth);
+[fefC] = histcounts(fefWidths,'BinLimits',[0 1],'BinWidth',binWidth);
+[scC] = histcounts(scWidths,'BinLimits',[0 1],'BinWidth',binWidth);
 
-binLims = [0 max([max(xWavWidths),max(yWavWidths)])+100];
+xLims = [0.05 0.7];
+units = wavWidthsTblStats.GroupCount;
+means = round(wavWidthsTblStats.mean_wavWidthSearchMean.*1000);
+stds = round(wavWidthsTblStats.std_wavWidthSearchMean.*1000);
+
+h_fig = figure;
+%% SEF
+subplot(3,1,1)
+h_sef = bar(be(2:end),sefC./sum(sefC),'BarWidth',1,'EdgeColor','none','FaceAlpha',0.5);
+xlim(xLims)
+xlabel('Spike width (ms)')
+ylabel('Normalized counts')
+title('SEF Spike width distribution')
+idxD = find(strcmp(wavWidthsTblStats.monkey,'D') & strcmp(wavWidthsTblStats.area,'SEF'));
+idxE = find(strcmp(wavWidthsTblStats.monkey,'E') & strcmp(wavWidthsTblStats.area,'SEF'));
+txt = {sprintf('%s(n=%d) %3d\\pm%2d', 'D', units(idxD), means(idxD), stds(idxD));...
+       sprintf('%s(n=%d) %3d\\pm%2d', 'E', units(idxE), means(idxE), stds(idxE))};
+yl=get(gca,'YLim');
+h_tsef =text(min(xLims)+0.05*range(xLims),max(yl)-0.25*range(yl),txt,'Interpreter','tex','FontWeight','bold');
+%% FEF only Darwin
+subplot(3,1,2)
+h_fef = bar(be(2:end),fefC./sum(fefC),'BarWidth',1,'EdgeColor','none','FaceAlpha',0.5);
+xlim(xLims)
+xlabel('Spike width (ms)')
+ylabel('Normalized counts')
+title('FEF Spike width distribution')
+idxD = find(strcmp(wavWidthsTblStats.monkey,'D') & strcmp(wavWidthsTblStats.area,'FEF'));
+txt = {sprintf('%s(n=%d) %3d\\pm%2d', 'D', units(idxD), means(idxD), stds(idxD));...
+       'E(n=0)'};
+yl=get(gca,'YLim');
+h_tfef =text(min(xLims)+0.05*range(xLims),max(yl)-0.25*range(yl),txt,'Interpreter','tex','FontWeight','bold');
+
+%% SC
+subplot(3,1,3)
+h_sc = bar(be(2:end),scC./sum(scC),'BarWidth',1,'EdgeColor','none','FaceAlpha',0.5);
+xlim(xLims)
+xlabel('Spike width (ms)')
+ylabel('Normalized counts')
+title('SC Spike width distribution')
+idxD = find(strcmp(wavWidthsTblStats.monkey,'D') & strcmp(wavWidthsTblStats.area,'SC'));
+idxE = find(strcmp(wavWidthsTblStats.monkey,'E') & strcmp(wavWidthsTblStats.area,'SC'));
+txt = {sprintf('%s(n=%d) %3d\\pm%2d', 'D', units(idxD), means(idxD), stds(idxD));...
+       sprintf('%s(n=%d) %3d\\pm%2d', 'E', units(idxE), means(idxE), stds(idxE))};
+yl=get(gca,'YLim');
+h_tsc =text(min(xLims)+0.05*range(xLims),max(yl)-0.25*range(yl),txt,'Interpreter','tex','FontWeight','bold');
+
 
 
 
