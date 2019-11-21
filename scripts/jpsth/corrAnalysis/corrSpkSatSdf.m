b9487000cf69f467e00a24ba9ec0e031087840d0
Chenchal Subraveti <chenchals@gmail.com>
2019-11-21 15:13:11 -0600

    added filter criteria for sdfs

--- scripts/jpsth/corrAnalysis/corrSpkSatSdf.m
+++ scripts/jpsth/corrAnalysis/corrSpkSatSdf.m
@@ -2,7 +2,8 @@
 % for paired neurons
 
 % Use static windows
-fn = 'dataProcessed/analysis/11-18-2019/spkCorr/summary/spkCorrAllPairsStaticNew.mat';
+spkCorrDir = 'dataProcessed/analysis/11-18-2019/spkCorr';
+fn = fullfile(spkCorrDir,'summary/spkCorrAllPairsStaticNew.mat');
 %spkCorr = load(fn);
 outPdfDir = 'dataProcessed/analysis/11-18-2019/spkCorr/summary/sdf';
 if ~exist(outPdfDir,'dir')
@@ -10,5 +11,37 @@ if ~exist(outPdfDir,'dir')
 end
 outPdfFn = fullfile(outPdfDir,'satSdf_');
 
+%% Fiter data for use too plot sdfs
+% Use following area pairs (these are fields of spkCorr loaded above)
+pairAreas = {'SEF_SEF','SEF_FEF','SEF_SC'}';
+spkCorrBase = cellfun(@(x) fullfile(spkCorrDir,['spkCorr_' x],'mat'),pairAreas,'UniformOutput',false);
+% Use following fields
+useCols = {
+    'srcFile'                      
+    'pairAreas'
+    'condition'
+    'alignedName'
+    'alignedEvent'
+    'nTrials'                      
+    'Pair_UID'                     
+    'X_monkey'                     
+    'X_sess'                       
+    'X_unitNum'                    
+    'Y_unitNum'                    
+    'rhoRaw_200ms'                 
+    'pvalRaw_200ms'                
+ };
+% Use significance level
+useSignif = 0.05;
+%%
+spkCorrAllTbl = table();
+for pa =1:numel(pairAreas)
+    temp = spkCorr.(pairAreas{pa})(:,useCols);
+    temp.srcFile = strcat(spkCorrBase{pa},filesep,temp.srcFile);
+    temp = temp(temp.pvalRaw_200ms>useSignif,:);
+    spkCorrAllTbl = [spkCorrAllTbl; temp];
+    clear temp
+end
 
 
+%%
