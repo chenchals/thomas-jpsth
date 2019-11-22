715cb31d08c3e8189319bbb880647baf187b3c3a
Chenchal Subraveti <chenchals@gmail.com>
2019-11-22 15:26:05 -0600

    Added SAT anova and annotate the results table

---
+++ scripts/jpsth/corrAnalysis/vssFig/corrSpkAnova.m
@@ -0,0 +1,36 @@
+% Run anova for different factors of SAT for spike correlations
+fn = 'dataProcessed/analysis/11-18-2019/spkCorr/summary/spkCorrAllPairsStaticNew.mat';
+%spkCorr = load(fn);
+%outPdfFn = 'dataProcessed/analysis/11-18-2019/spkCorr/summary/crossAreaCompare';
+useAbsRho = 1; % no support for useAbsRho = 0
+%% aggregate required fields into a single table
+pairAreas = {'SEF_SEF','SEF_FEF','SEF_SC'};
+% Prune to columns of interest for each pairArea
+useCols = {
+    'pairAreas'
+    'XY_Dist'
+    'condition'
+    'alignedName'
+    'rhoRaw_200ms'
+    };
+
+spkCorrAllTbl = table();
+for pa =1:numel(pairAreas)
+    pairArea = pairAreas{pa};
+    spkCorrAllTbl = [spkCorrAllTbl; spkCorr.(pairArea)(:,useCols)];
+end
+%% Filter data and assemble values and factors into a table
+% Filter data --> remove all values where XY_Dist = 0
+spkCorrAllTbl = spkCorrAllTbl(spkCorrAllTbl.XY_Dist~=0,:);
+if useAbsRho
+    spkCorrAllTbl.rhoRaw_200ms = abs(spkCorrAllTbl.rhoRaw_200ms);
+end
+% Recode groups/factors for anova
+valsGroupsTbl = table();
+valsGroupsTbl.yVals = spkCorrAllTbl.rhoRaw_200ms;
+valsGroupsTbl.condition = regexprep(spkCorrAllTbl.condition,'Correct|Error.*','');
+valsGroupsTbl.epoch = spkCorrAllTbl.alignedName;
+
+%%
+[anovaResults] = satAnova(valsGroupsTbl);
+
---
+++ scripts/jpsth/corrAnalysis/vssFig/satAnova.m
@@ -0,0 +1,58 @@
+function [anovaResults] = satAnova(valsGroupsTbl)
+%SATANOVA Do a multiway anova for the given table of inputs
+%   Column1 = Y-Values numeric
+%   Columns (2 to end) = groups/factors over which the anonan is run
+% ***Note prefer to use group to factor since factor is a function in
+% matlab
+% ***Note: dont use "factor" *** --> Its is a matlab funtion for primes  doc FACTOR
+% see also ANOVAN, MULTCOMPARE
+
+    yVals = valsGroupsTbl{:,1};
+    assert(isnumeric(yVals),'Y-Values must be numeric');
+    
+    groupNames = valsGroupsTbl.Properties.VariableNames;
+    groupNames = groupNames(2:end);
+    
+    groups = cell(1,numel(groupNames));
+    for ii = 1:numel(groupNames)
+        groups{ii} = valsGroupsTbl.(groupNames{ii});
+    end
+    % anovaTbl header:
+    % {'Source' 'Sum Sq.' 'd.f.' 'Singular?' 'Mean Sq.' 'F'  'Prob>F'}
+    anovaResults = struct();
+    anovaTblVarNames = {'Source', 'SumSq' 'df' 'IsSingular' 'MeanSq' 'F'  'ProbGtF'};
+    
+    [~,temp,anovaStats] = anovan(yVals,groups,'model','linear','varnames',groupNames);
+    anovaResults.anovaTbl = cell2table(temp(2:end,:),'VariableNames',anovaTblVarNames);
+    
+    % Compare results for different LEVELS *WITHIN* each group/Factor independently    
+    for gr = 1:numel(groupNames)   
+        [temp,~,~,grpNames] = multcompare(anovaStats,'Dimension',gr,'Alpha',0.05);
+        anovaResults.(groupNames{gr}) = annotateMultcompareResults(temp,grpNames);
+    end
+    
+    % Compare results for different LEVEL combinations *ACROSS* each group/Factor independently
+    nGrpComparisions = combnk(1:numel(groupNames),2);
+    
+    for jj = 1:size(nGrpComparisions,1)
+        idx = nGrpComparisions(jj,:);
+        fn = [groupNames{idx(1)} '_' groupNames{idx(2)}];
+        [temp,~,~,grpNames] = multcompare(anovaStats,'Dimension',1:numel(groupNames),'Alpha',0.05);
+        anovaResults.(fn) = annotateMultcompareResults(temp,grpNames);
+    end
+end
+function [resultsAsTbl] = annotateMultcompareResults(results,grpNames)
+% see also multcompare
+    levelNamesNew = split(grpNames,{',','='});
+    levelNamesNew = levelNamesNew(:,2:2:end);
+    levelNamesNew = arrayfun(@(x) char(join(levelNamesNew(x,:),'_')),1:size(levelNamesNew,1),'UniformOutput',false)';
+    
+    resultsAsTbl = table();
+    resultsAsTbl.levelName1 = arrayfun(@(x) levelNamesNew(x),results(:,1),'UniformOutput',false);
+    resultsAsTbl.levelName2 = arrayfun(@(x) levelNamesNew(x),results(:,2),'UniformOutput',false);
+    resultsAsTbl = [resultsAsTbl array2table(results(:,3:end),'VariableNames',{'loCI','meanDiff','hiCI','pval_H0'})];
+    resultsAsTbl.isSignif05 = results(:,end)<=0.05;
+    resultsAsTbl.group1 = results(:,1);
+    resultsAsTbl.group2 = results(:,2);
+end
+
