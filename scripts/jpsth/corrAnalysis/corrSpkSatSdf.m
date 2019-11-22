fd06c6700ca079047c2f11e17849db98aa6512ae
Chenchal Subraveti <chenchals@gmail.com>
2019-11-22 10:23:34 -0600

    updated for new VSS fig....

--- scripts/jpsth/corrAnalysis/corrSpkSatSdf.m
+++ scripts/jpsth/corrAnalysis/corrSpkSatSdf.m
@@ -4,7 +4,7 @@
 % Use static windows
 spkCorrDir = 'dataProcessed/analysis/11-18-2019/spkCorr';
 fn = fullfile(spkCorrDir,'summary/spkCorrAllPairsStaticNew.mat');
-%spkCorr = load(fn);
+spkCorr = load(fn);
 outPdfDir = 'dataProcessed/analysis/11-18-2019/spkCorr/summary/sdf';
 if ~exist(outPdfDir,'dir')
     mkdir(outPdfDir);
@@ -27,21 +27,33 @@ useCols = {
     'X_monkey'                     
     'X_sess'                       
     'X_unitNum'                    
-    'Y_unitNum'                    
+    'Y_unitNum'
+    'X_area'
+    'Y_area'
     'rhoRaw_200ms'                 
     'pvalRaw_200ms'                
  };
 % Use significance level
 useSignif = 0.05;
+% Use rho percentile
+useRhoPercentile = 70;
 %%
 spkCorrAllTbl = table();
-for pa =1:numel(pairAreas)
+for pa = 1:numel(pairAreas)
     temp = spkCorr.(pairAreas{pa})(:,useCols);
     temp.srcFile = strcat(spkCorrBase{pa},filesep,temp.srcFile);
-    temp = temp(temp.pvalRaw_200ms>useSignif,:);
+%     temp.isSignifByEpoch = temp.pvalRaw_200ms <= useSignif;
+%     temp.isSignifPair = cellfun(@(x) sum(temp.isSignifByEpoch(strcmp(temp.Pair_UID,x))) > 0,temp.Pair_UID);
     spkCorrAllTbl = [spkCorrAllTbl; temp];
     clear temp
 end
+%% Get unique units list
+unitTbl = table();
+
+unitTbl.unitNum = [spkCorrAllTbl.X_unitNum;spkCorrAllTbl.Y_unitNum];
+unitTbl.area = [spkCorrAllTbl.X_area;spkCorrAllTbl.Y_area];
+unitTbl = unique(unitTbl,'stable');
+
 
 
 %%
