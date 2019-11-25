b502738c5b81b75b5185fbf3d31e3b0d4ca073bd
Chenchal Subraveti <chenchals@gmail.com>
2019-11-25 14:19:13 -0600

    Updated to filter pairs for area wise, +, -, both spk corr by unit number

--- scripts/jpsth/corrAnalysis/corrSpkSatSdf.m
+++ scripts/jpsth/corrAnalysis/corrSpkSatSdf.m
@@ -33,27 +33,91 @@ useCols = {
     'rhoRaw_200ms'                 
     'pvalRaw_200ms'                
  };
-% Use significance level
-useSignif = 0.05;
-% Use rho percentile
-useRhoPercentile = 70;
 %%
 spkCorrAllTbl = table();
 for pa = 1:numel(pairAreas)
-    temp = spkCorr.(pairAreas{pa})(:,useCols);
-    temp.srcFile = strcat(spkCorrBase{pa},filesep,temp.srcFile);
+    unitsPlus = spkCorr.(pairAreas{pa})(:,useCols);
+    unitsPlus.srcFile = strcat(spkCorrBase{pa},filesep,unitsPlus.srcFile);
 %     temp.isSignifByEpoch = temp.pvalRaw_200ms <= useSignif;
 %     temp.isSignifPair = cellfun(@(x) sum(temp.isSignifByEpoch(strcmp(temp.Pair_UID,x))) > 0,temp.Pair_UID);
-    spkCorrAllTbl = [spkCorrAllTbl; temp];
+    spkCorrAllTbl = [spkCorrAllTbl; unitsPlus];
     clear temp
 end
-%% Get unique units list
-unitTbl = table();
 
-unitTbl.unitNum = [spkCorrAllTbl.X_unitNum;spkCorrAllTbl.Y_unitNum];
-unitTbl.area = [spkCorrAllTbl.X_area;spkCorrAllTbl.Y_area];
-unitTbl = unique(unitTbl,'stable');
+%% Filter criteria
+% Use epoch
+useEpoch = 'PostSaccade';
+% Use significance level
+useSignif = 0.01;
+% Use rho percentile
+useRhoPercentile = 90;
+
+isEpoch = strcmp(spkCorrAllTbl.alignedName,useEpoch);
+isSignifIdx = spkCorrAllTbl.pvalRaw_200ms <= useSignif;
+plusIdx = spkCorrAllTbl.rhoRaw_200ms>=0;
+minusIdx = spkCorrAllTbl.rhoRaw_200ms<0;
+
+hiPlus = prctile(spkCorrAllTbl.rhoRaw_200ms(plusIdx & isEpoch),useRhoPercentile);
+loMinus = prctile(spkCorrAllTbl.rhoRaw_200ms(minusIdx & isEpoch),useRhoPercentile);
+
+hiPlusSignifIdx = spkCorrAllTbl.rhoRaw_200ms>=hiPlus & isSignifIdx & isEpoch;
+loMinusSignifIdx = spkCorrAllTbl.rhoRaw_200ms<=loMinus & isSignifIdx & isEpoch;
 
 
+%% Get units list for + and - spike corrs
+
+plusTable = spkCorrAllTbl(hiPlusSignifIdx,:);
+minusTable = spkCorrAllTbl(loMinusSignifIdx,:);
+% plus spk corr table
+
+unitsPlus = table();
+unitsPlus.unitNum = [plusTable.X_unitNum;plusTable.Y_unitNum];
+unitsPlus.area = [plusTable.X_area;plusTable.Y_area];
+unitsPlus.threshPlus = repmat(hiPlus,size(unitsPlus,1),1);
+unitsPlus = sortrows(unique(unitsPlus,'stable'),'area');
+% minus spk corr table
+unitsMinus = table();
+unitsMinus.unitNum = [minusTable.X_unitNum;minusTable.Y_unitNum];
+unitsMinus.area = [minusTable.X_area;minusTable.Y_area];
+unitsMinus.threshMinus = repmat(loMinus,size(unitsMinus,1),1);
+unitsMinus = sortrows(unique(unitsMinus,'stable'),'area');
 
 %%
+unitTbl = table();
+uniqUnits = unique([unitsPlus.unitNum;unitsMinus.unitNum]);
+
+for ii = 1:numel(uniqUnits)
+    unitNum = uniqUnits(ii);
+    pIdx = max([find(unitsPlus.unitNum==unitNum),0]);
+    mIdx = max([find(unitsMinus.unitNum==unitNum),0]);
+    temp = table();
+    if pIdx && ~mIdx
+        temp.unitNum = unitNum;
+        temp.area = unitsPlus.area(pIdx);
+        temp.threshPlus = unitsPlus.threshPlus(pIdx);
+        temp.threshMinus = NaN;
+        temp.isPlus = 1;
+        temp.isMinus = 0;
+        temp.isBoth = 0;
+    elseif mIdx && ~pIdx
+        temp.unitNum = unitNum;
+        temp.area = unitsMinus.area(mIdx);
+        temp.threshPlus = NaN;
+        temp.threshMinus = unitsMinus.threshMinus(mIdx); 
+        temp.isPlus = 0;
+        temp.isMinus = 1;
+        temp.isBoth = 0;
+    elseif pIdx && mIdx
+        temp.unitNum = unitNum;
+        temp.area = unitsMinus.area(pIdx);
+        temp.threshPlus = unitsPlus.threshPlus(pIdx);
+        temp.threshMinus = unitsMinus.threshMinus(mIdx);  
+        temp.isPlus = 0;
+        temp.isMinus = 0;
+        temp.isBoth = 1;
+    end
+    unitTbl = [unitTbl;temp];
+end
+
+Z = sortrows(unitTbl,{'area','isPlus','isMinus','isBoth','unitNum'});
+
