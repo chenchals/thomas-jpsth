210855f9b1fcfceb53c144b0e1445ed888fdb33a
Thomas Reppert <thomas.reppert@gmail.com>
2019-12-04 15:28:45 -0600

    Added plotting to corrSpkAnova()

--- scripts/jpsth/corrAnalysis/vssFig/corrSpkAnova.m
+++ scripts/jpsth/corrAnalysis/vssFig/corrSpkAnova.m
@@ -4,55 +4,105 @@
 % of variance with factors Trial Epoch (4 levels) and Task Condition (2
 % levels).
 
-areaTest = 'SEF-FEF';
+windowTest = 'rhoRaw_150ms';
 trialOutcome = {'Correct','ErrorChoice','ErrorTiming'};
-% outFn = 'dataProcessed/analysis/11-18-2019/spkCorr/summary/spkCorrAnovaSummary.mat';
+ylimPlot = [0.04 0.18];
 
-%% Run anova for different factors of SAT for spike correlations
-spkCorr = load('dataProcessed/analysis/11-18-2019/spkCorr/summary/spkCorrAllPairsStaticNew.mat');
+%load r_sc data
+% load('dataProcessed/analysis/11-18-2019/spkCorr/summary/spkCorrAllPairsStaticNew.mat', 'SEF_FEF','SEF_SC','SEF_SEF');
 
 %% Aggregate required fields into a single table
-pairAreas = {'SEF_SEF','SEF_FEF','SEF_SC'};
+% pairAreas = {'SEF_SEF','SEF_FEF','SEF_SC'};
 % Prune to columns of interest for each pairArea
-useCols = {'pairAreas','XY_Dist','condition','alignedName','rhoRaw_200ms'};
+useCols = {'pairAreas','XY_Dist','condition','alignedName',windowTest};
 
-spkCorr_All = table();
-for pa =1:numel(pairAreas)
-    pairArea = pairAreas{pa};
-    spkCorr_All = [spkCorr_All; spkCorr.(pairArea)(:,useCols)];
-end
+%isolate area pairing of interest
+% spkCorr = SEF_SEF(:,useCols);
+spkCorr = [SEF_FEF(:,useCols); SEF_SC(:,useCols)];
+areaPair = 'SEF-FEF & SEF-SC';
+
+%take absolute value of correlation
+spkCorr.(windowTest) = abs(spkCorr.(windowTest));
 
 %remove all values where XY_Dist = 0 -- commented by TR 2019-12-02
 % spkCorr_All = spkCorr_All(spkCorr_All.XY_Dist~=0,:);
 
-%take absolute value of correlation
-spkCorr_All.rhoRaw_200ms = abs(spkCorr_All.rhoRaw_200ms);
+%initialize output for plot and ANOVA
+rsc_Acc = cell(1,length(trialOutcome));
+rsc_Fast = cell(1,length(trialOutcome));
+stats_Anova = cell(1,length(trialOutcome));
 
+rsc_Baseline_Acc = NaN(2,length(trialOutcome));
+rsc_Baseline_Fast = NaN(2,length(trialOutcome));
 
-for to = 1:3
+figure()
 
-if (USE_UNSIGNED_RHO)
-    
-end
+for jj = 1:3
+  
+  %index by trial outcome
+  idx_jj = ismember(spkCorr.condition, {['Accurate' trialOutcome{jj}],['Fast' trialOutcome{jj}]});  
+  spkCorr_jj = spkCorr(idx_jj,:);
+  
+  %ANOVA -- Factors (groups) Condition (2) by Epoch (4)
+  valsGroupsTbl = table();
+  valsGroupsTbl.yVals = spkCorr_jj.(windowTest);
+  valsGroupsTbl.condition = regexprep(spkCorr_jj.condition,'Correct|Error.*',''); %regular expression replace
+  valsGroupsTbl.epoch = spkCorr_jj.alignedName;
+  stats_Anova{jj} = satAnova(valsGroupsTbl);
+  
+  %prepare r_sc for plotting (index by epoch and task condition)
+  idx_Acc = ismember(spkCorr_jj.condition, ['Accurate' trialOutcome{jj}]);
+  idx_Fast = ismember(spkCorr_jj.condition, ['Fast' trialOutcome{jj}]);
+  
+  idx_Baseline = ismember(spkCorr_jj.alignedName, 'Baseline');
+  idx_Visual = ismember(spkCorr_jj.alignedName, 'Visual');
+  idx_PostSacc = ismember(spkCorr_jj.alignedName, 'PostSaccade');
+  idx_PostRew = ismember(spkCorr_jj.alignedName, 'PostReward');
+  
+  %collect r_sc for plotting
+  rsc_jj = spkCorr_jj.(windowTest);
+  rsc_Acc{jj} = [rsc_jj(idx_Acc & idx_Baseline) , rsc_jj(idx_Acc & idx_Visual) , rsc_jj(idx_Acc & idx_PostSacc) , rsc_jj(idx_Acc & idx_PostRew)];
+  rsc_Fast{jj} = [rsc_jj(idx_Fast & idx_Baseline) , rsc_jj(idx_Fast & idx_Visual) , rsc_jj(idx_Fast & idx_PostSacc) , rsc_jj(idx_Fast & idx_PostRew)];
+  
+  %compute mean and s.e.
+  rsc_Acc_mu = nanmean(rsc_Acc{jj});
+  rsc_Fast_mu = nanmean(rsc_Fast{jj});
+  rsc_Acc_se = nanstd(rsc_Acc{jj}) / sqrt(size(rsc_Acc{jj},1));
+  rsc_Fast_se = nanstd(rsc_Fast{jj}) / sqrt(size(rsc_Fast{jj},1));
+  
+  rsc_Baseline_Acc(:,jj) = [rsc_Acc_mu(1); rsc_Acc_se(1)];
+  rsc_Baseline_Fast(:,jj) = [rsc_Fast_mu(1); rsc_Fast_se(1)];
+  
+  subplot(3,2,2*jj-1) %Accurate
+  errorbar((1:4), rsc_Acc_mu, rsc_Acc_se, 'CapSize',0, 'Color','r')
+  xticks(1:4); xticklabels([]); xlim([.8 4.2]); ylim(ylimPlot); ytickformat('%3.2f')
+  
+  subplot(3,2,2*jj) %Fast
+  errorbar((1:4), rsc_Fast_mu, rsc_Fast_se, 'CapSize',0, 'Color',[0 .7 0])
+  xticks(1:4); xticklabels([]); xlim([.8 4.2]); ylim(ylimPlot); yticks([])
+  
+  pause(0.25)
+  
+end % for : trialOutcome (jj)
 
-idxTrialOutcome = ismember(spkCorr_All.condition, {'AccurateErrorChoice','FastErrorChoice'});
-idxArea = strcmp(spkCorr_All.pairAreas,areaTest);
-spkCorrFilt = spkCorr_All(idxTrialOutcome & idxArea,:);
+title(areaPair)
+ppretty([6.4,4])
 
-%% Recode groups/factors for anova - CONDITION (2) by EPOCH (4) = (8*7)/2 = 28 comparisions
-valsGroupsTbl = table();
-valsGroupsTbl.yVals = spkCorrFilt.rhoRaw_200ms;
-valsGroupsTbl.condition = regexprep(spkCorrFilt.condition,'Correct|Error.*','');
-valsGroupsTbl.epoch = spkCorrFilt.alignedName;
-[conditionByEpoch] = satAnova(valsGroupsTbl);
-end % for : trialOutcome (to)
+%summary barplot
+figure(); hold on
+bar((1:2:5), rsc_Baseline_Acc(1,:), 'FaceColor','r', 'BarWidth',0.35)
+errorbar((1:2:5), rsc_Baseline_Acc(1,:), rsc_Baseline_Acc(2,:), 'CapSize',0, 'Color','k')
+bar((2:2:6), rsc_Baseline_Fast(1,:), 'FaceColor',[0 .7 0], 'BarWidth',0.35)
+errorbar((2:2:6), rsc_Baseline_Fast(1,:), rsc_Baseline_Fast(2,:), 'CapSize',0, 'Color','k')
+xticks([]); ytickformat('%3.2f')
+ppretty([3,3])
+
+clear idx_* rsc_* *jj
 
 %% Save data
 %save(outFn,'conditionByEpoch','pairAreasByEpoch');
 
-
-
-% %% Recode groups/factors for anova - PAIRAREA (3) by EPOCH (4) = (12*11)/2 = 66 comparisions
+%% Recode groups/factors for anova - PAIRAREA (3) by EPOCH (4) = (12*11)/2 = 66 comparisions
 % valsGroupsTbl = table();
 % valsGroupsTbl.yVals = spkCorrFilt.rhoRaw_200ms;
 % valsGroupsTbl.pairAreas = spkCorrFilt.pairAreas;
--- scripts/jpsth/corrAnalysis/vssFig/satAnova.m
+++ scripts/jpsth/corrAnalysis/vssFig/satAnova.m
@@ -2,81 +2,84 @@ function [anovaResults] = satAnova(valsGroupsTbl)
 %SATANOVA Do a multiway anova for the given table of inputs
 %   Column1 = Y-Values numeric
 %   Columns (2 to end) = groups/factors over which the anonan is run
-% ***Note prefer to use group to factor since factor is a function in
-% matlab
+% ***Terminology: group == factor
 % ***Note: dont use "factor" *** --> Its is a matlab funtion for primes  doc FACTOR
 % see also ANOVAN, MULTCOMPARE
-    model_Anova = 'linear'; %'interaction'
-    
-    yVals = valsGroupsTbl{:,1};
-    assert(isnumeric(yVals),'Y-Values must be numeric');
-    
-    groupNames = valsGroupsTbl.Properties.VariableNames;
-    groupNames = groupNames(2:end);
-    
-    groups = cell(1,numel(groupNames));
-    for ii = 1:numel(groupNames)
-        groups{ii} = valsGroupsTbl.(groupNames{ii});
-    end
-    % anovaTbl header:
-    % {'Source' 'Sum Sq.' 'd.f.' 'Singular?' 'Mean Sq.' 'F'  'Prob>F'}
-    anovaResults = struct();
-    anovaTblVarNames = {'Source', 'SumSq' 'df' 'IsSingular' 'MeanSq' 'F'  'ProbGtF'};
-    
-    [~,temp,anovaStats] = anovan(yVals,groups,'model',model_Anova,'varnames',groupNames);
-    anovaResults.anovaTbl = cell2table(temp(2:end,:),'VariableNames',anovaTblVarNames);
-    
-    % Compare results for different LEVELS *WITHIN* each group/Factor independently    
-    for gr = 1:numel(groupNames)   
-        [temp,~,~,grpNames] = multcompare(anovaStats,'Dimension',gr,'Alpha',0.05);
-        anovaResults.(groupNames{gr}) = annotateMultcompareResults(temp,grpNames);
-    end
-    
-    %% Compare results for different LEVEL combinations *ACROSS* each group/Factor independently
-    nWays = 2;
-    n2GrpComparisions = combnk(1:numel(groupNames),nWays);
-    
-    for jj = 1:size(n2GrpComparisions,1)
-        idx = n2GrpComparisions(jj,:);
+
+model_Anova = 'linear'; %'interaction'
+performMultCompare = false;
+anovaDisplay = 'off'; %'on'
+
+yVals = valsGroupsTbl{:,1};
+assert(isnumeric(yVals),'Y-Values must be numeric');
+
+groupNames = valsGroupsTbl.Properties.VariableNames;
+groupNames = groupNames(2:end);
+
+groups = cell(1,numel(groupNames));
+for ii = 1:numel(groupNames)
+    groups{ii} = valsGroupsTbl.(groupNames{ii});
+end
+
+% anovaTbl header:
+% {'Source' 'Sum Sq.' 'd.f.' 'Singular?' 'Mean Sq.' 'F'  'Prob>F'}
+anovaResults = struct();
+anovaTblVarNames = {'Source', 'SumSq' 'df' 'IsSingular' 'MeanSq' 'F'  'ProbGtF'};
+
+[~,temp,anovaStats] = anovan(yVals,groups,'model',model_Anova,'varnames',groupNames, 'display',anovaDisplay);
+anovaResults.anovaTbl = cell2table(temp(2:end,:),'VariableNames',anovaTblVarNames);
+
+% Compare results for different LEVELS *WITHIN* each group/Factor independently    
+if (performMultCompare)
+for gr = 1:numel(groupNames)   
+    [temp,~,~,grpNames] = multcompare(anovaStats,'Dimension',gr,'Alpha',0.05);
+    anovaResults.(groupNames{gr}) = annotateMultcompareResults(temp,grpNames);
+end
+
+% Compare results for different LEVEL combinations *ACROSS* each group/Factor independently
+  nWays = 2;
+  n2GrpComparisions = combnk(1:numel(groupNames),nWays);
+
+  for jj = 1:size(n2GrpComparisions,1)
+      idx = n2GrpComparisions(jj,:);
+      fn = char(join(groupNames(idx),'_'));
+      [temp,~,~,grpNames] = multcompare(anovaStats,'Dimension',idx,'Alpha',0.05);
+      anovaResults.(fn) = annotateMultcompareResults(temp,grpNames);
+  end
+end
+
+%% If there are 3 groups also do a 3 way comparision
+if numel(groupNames) > 2
+    nWays = 3;
+    n3GrpComparisions = combnk(1:numel(groupNames),nWays);
+
+    for jj = 1:size(n3GrpComparisions,1)
+        idx = n3GrpComparisions(jj,:);
         fn = char(join(groupNames(idx),'_'));
         [temp,~,~,grpNames] = multcompare(anovaStats,'Dimension',idx,'Alpha',0.05);
         anovaResults.(fn) = annotateMultcompareResults(temp,grpNames);
     end
-    
-    %% If there are 3 groups also do a 3 way comparision
-    if numel(groupNames) > 2
-        nWays = 3;
-        n3GrpComparisions = combnk(1:numel(groupNames),nWays);
-        
-        for jj = 1:size(n3GrpComparisions,1)
-            idx = n3GrpComparisions(jj,:);
-            fn = char(join(groupNames(idx),'_'));
-            [temp,~,~,grpNames] = multcompare(anovaStats,'Dimension',idx,'Alpha',0.05);
-            anovaResults.(fn) = annotateMultcompareResults(temp,grpNames);
-        end
-        
-    end
-    
-    
-    
-    
-end
-function [resultsAsTbl] = annotateMultcompareResults(results,grpNames)
-% see also multcompare
-    levelNamesNew = split(grpNames,{',','='});
-    levelNamesNew = levelNamesNew(:,2:2:end);
-    levelNamesNew = arrayfun(@(x) char(join(levelNamesNew(x,:),'_')),1:size(levelNamesNew,1),'UniformOutput',false)';
-    
-    resultsAsTbl = table();
-    resultsAsTbl.levelName1 = arrayfun(@(x) levelNamesNew(x),results(:,1),'UniformOutput',false);
-    resultsAsTbl.levelName2 = arrayfun(@(x) levelNamesNew(x),results(:,2),'UniformOutput',false);
-    resultsAsTbl = [resultsAsTbl array2table(results(:,3:end),'VariableNames',{'loCI95','meanDiff','hiCI95','pval_H0'})];
-    resultsAsTbl.isSignif05 = results(:,end)<=0.05;
-    resultsAsTbl.isSignif01 = results(:,end)<=0.01;
-    resultsAsTbl.signifStr=repmat({''},size(resultsAsTbl,1),1);
-    resultsAsTbl.signifStr(resultsAsTbl.isSignif05) = repmat({'*'},sum(resultsAsTbl.isSignif05),1);
-    resultsAsTbl.signifStr(resultsAsTbl.isSignif01) = repmat({'**'},sum(resultsAsTbl.isSignif01),1);
-    resultsAsTbl.group1 = results(:,1);
-    resultsAsTbl.group2 = results(:,2);
 end
 
+end % fxn : satAnova()
+
+function [resultsAsTbl] = annotateMultcompareResults(results,grpNames)
+% see also multcompare
+
+levelNamesNew = split(grpNames,{',','='});
+levelNamesNew = levelNamesNew(:,2:2:end);
+levelNamesNew = arrayfun(@(x) char(join(levelNamesNew(x,:),'_')),1:size(levelNamesNew,1),'UniformOutput',false)';
+
+resultsAsTbl = table();
+resultsAsTbl.levelName1 = arrayfun(@(x) levelNamesNew(x),results(:,1),'UniformOutput',false);
+resultsAsTbl.levelName2 = arrayfun(@(x) levelNamesNew(x),results(:,2),'UniformOutput',false);
+resultsAsTbl = [resultsAsTbl array2table(results(:,3:end),'VariableNames',{'loCI95','meanDiff','hiCI95','pval_H0'})];
+resultsAsTbl.isSignif05 = results(:,end)<=0.05;
+resultsAsTbl.isSignif01 = results(:,end)<=0.01;
+resultsAsTbl.signifStr=repmat({''},size(resultsAsTbl,1),1);
+resultsAsTbl.signifStr(resultsAsTbl.isSignif05) = repmat({'*'},sum(resultsAsTbl.isSignif05),1);
+resultsAsTbl.signifStr(resultsAsTbl.isSignif01) = repmat({'**'},sum(resultsAsTbl.isSignif01),1);
+resultsAsTbl.group1 = results(:,1);
+resultsAsTbl.group2 = results(:,2);
+
+end % util : annotateMultcompareResults()
