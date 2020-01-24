f95d40d899e046e5fa63a93e596096f3e1fe37f3
chenchals <chenchals@gmail.com>
2020-01-23 23:07:33 -0600

    Updated to plot heatmaps after filtering for median baseline Fr for units in an area

--- scripts/paperFigs/sdfSummAllNeurons/sdfHeatmapsUberScript.m
+++ scripts/paperFigs/sdfSummAllNeurons/sdfHeatmapsUberScript.m
@@ -12,25 +12,102 @@
     end
     clearvars temp fns jj
 %% Plot all units by significance noFrOrSign filter
-tic
-unitsNoFilterTbl = categorizeUnitsByRscSignif(allSpkCorr);
-useNormalized = 0; subDirName = 'noFilterMean';
-[sdfsNoFilterMean,sdfsImgsNoFilterMean] = plotHeatmapsNoFilter(unitsNoFilterTbl,useNormalized,subDirName);
-useNormalized = 1; subDirName = 'noFilterNorm';
-[sdfsNoFilter,sdfsImgsNoFilter] = plotHeatmapsNoFilter(unitsNoFilterTbl,useNormalized,subDirName);
-toc
+% tic
+% unitsNoFilterTbl = categorizeUnitsByRscSignif(allSpkCorr);
+% useNormalized = 0; subDirName = 'noFilterMean';
+% [sdfsNoFilterMean,sdfsImgsNoFilterMean] = plotHeatmapsNoFilter(unitsNoFilterTbl,useNormalized,subDirName);
+% useNormalized = 1; subDirName = 'noFilterNorm';
+% [sdfsNoFilter,sdfsImgsNoFilter] = plotHeatmapsNoFilter(unitsNoFilterTbl,useNormalized,subDirName);
+% toc
 %% filter for Baseline firing rate for SEF and FEF cells
+parpoolSize = 0;
+if ~isempty(gcp('nocreate'))
+    parpoolSize = Inf;
+end
+% get all units
 unitsList = unique([allSpkCorr.X_unitNum;allSpkCorr.Y_unitNum]);
 blFrWin = [-600 0];
 sdfsDir = 'dataProcessed/dataset/satSdfs';
-for un = 1:numel(unitsList)
+unitsFrTbl = table();
+% Compiute mean baseline firing rate for each unit
+parfor (un = 1:numel(unitsList),parpoolSize)
+   fr = table();
    unitNum = unitsList(un);
    fn = fullfile(sdfsDir,num2str(unitNum,'Unit_%03d.mat'));
    temp = load(fn,'sdfs');
    ts = temp.sdfs.Visual_timeMs{1};
-   idx = find(ts>= blFrWin(1) & ts <= blFrWin(2));
+   idxX = find(ts>= blFrWin(1) & ts <= blFrWin(2)); %#ok<*PFBNS>
    temp = cat(1,temp.sdfs.Visual_sdfByTrial{:});
-   temp = temp(:,idx);
-   fr = mean(mean(temp));
-   
+   temp = temp(:,idxX);
+   fr.unitNum = unitNum;
+   fr.meanBlFr = mean(mean(temp,2));
+   unitsFrTbl = [unitsFrTbl;fr];
 end
+
+% add units' mean Baseline FR to the allSpkCorr X_units and Y_units
+% do for X_unitNums
+allSpkCorr=innerjoin(allSpkCorr,unitsFrTbl,'LeftKeys','X_unitNum','RightKeys','unitNum',...
+    'RightVariables','meanBlFr');
+allSpkCorr.X_meanBlFr = tempX.meanBlFr;
+allSpkCorr.meanBlFr = [];
+% do for Y_unitNums
+allSpkCorr=innerjoin(allSpkCorr,unitsFrTbl,'LeftKeys','Y_unitNum','RightKeys','unitNum',...
+    'RightVariables','meanBlFr');
+allSpkCorr.Y_meanBlFr = allSpkCorr.meanBlFr;
+allSpkCorr.meanBlFr = [];
+% Find median Baseline FR for SEF, FEF, SC
+unitAreas = {'SEF','FEF','SC'};
+medianBlFr = struct();
+for aa = 1:numel(unitAreas)
+    unitArea = unitAreas{aa};
+    idxX = ismember(allSpkCorr.X_area,unitArea);
+    idxY = ismember(allSpkCorr.Y_area,unitArea);    
+    %unitNums = unique(allSpkCorr.X_unitNum(idx),'stable');
+    %zz= arrayfun(@(x) unique(allSpkCorr.X_meanBlFr(allSpkCorr.X_unitNum==x)),unitNums);
+    medianBlFr.(unitArea) = median([unique(allSpkCorr.X_meanBlFr(idxX),'stable');...
+                                    unique(allSpkCorr.Y_meanBlFr(idxY),'stable')]);
+end
+
+% Filter allSpkCorr by median balseline FR for each type of pairs
+% SEF-FEF pairs filter for both SEF and FEF
+idxGt_Sef_Fef = allSpkCorr.X_meanBlFr >= medianBlFr.SEF & allSpkCorr.Y_meanBlFr >= medianBlFr.FEF ...
+      & ismember(allSpkCorr.X_area,'SEF') & ismember(allSpkCorr.Y_area,'FEF');
+idxLt_Sef_Fef = allSpkCorr.X_meanBlFr < medianBlFr.SEF & allSpkCorr.Y_meanBlFr < medianBlFr.FEF ...
+      & ismember(allSpkCorr.X_area,'SEF') & ismember(allSpkCorr.Y_area,'FEF');
+% SEF-SC pairs filter only for SEF and NOT SC
+idxGt_Sef_Sc = allSpkCorr.X_meanBlFr >= medianBlFr.SEF...
+      & ismember(allSpkCorr.X_area,'SEF') & ismember(allSpkCorr.Y_area,'SC');
+idxLt_Sef_Sc = allSpkCorr.X_meanBlFr < medianBlFr.SEF...
+      & ismember(allSpkCorr.X_area,'SEF') & ismember(allSpkCorr.Y_area,'SC');
+% SEF-SEF pairs filter for both X_SEF and Y_SEF
+idxGt_Sef_Sef = allSpkCorr.X_meanBlFr >= medianBlFr.SEF & allSpkCorr.Y_meanBlFr >= medianBlFr.SEF ...
+      & ismember(allSpkCorr.X_area,'SEF') & ismember(allSpkCorr.Y_area,'SEF');
+idxLt_Sef_Sef = allSpkCorr.X_meanBlFr < medianBlFr.SEF & allSpkCorr.Y_meanBlFr < medianBlFr.SEF ...
+      & ismember(allSpkCorr.X_area,'SEF') & ismember(allSpkCorr.Y_area,'SEF');
+medianFrGtTbl = [allSpkCorr(idxGt_Sef_Fef,:);allSpkCorr(idxGt_Sef_Sc,:);allSpkCorr(idxGt_Sef_Sef,:)];
+medianFrLtTbl = [allSpkCorr(idxLt_Sef_Fef,:);allSpkCorr(idxLt_Sef_Sc,:);allSpkCorr(idxLt_Sef_Sef,:)];
+% process heatmaps for median gt table
+unitsMedianFrGtTbl = categorizeUnitsByRscSignif(medianFrGtTbl);
+useNormalized = 1; subDirName = 'medianFrGtNorm';
+[sdfsMedianFrGt,sdfsImgsMedianFrGt] = plotHeatmapsNoFilter(unitsMedianFrGtTbl,useNormalized,subDirName);
+% process heatmaps for median lt table
+unitsMedianFrLtTbl = categorizeUnitsByRscSignif(medianFrLtTbl);
+useNormalized = 1; subDirName = 'medianFrLtNorm';
+[sdfsMedianFrLt,sdfsImgsMedianFrLt] = plotHeatmapsNoFilter(unitsMedianFrLtTbl,useNormalized,subDirName);
+
+%%
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
