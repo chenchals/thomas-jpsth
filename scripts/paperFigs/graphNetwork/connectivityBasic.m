149c604a2f3f7fd402bdb74ce8206aafab84437e
Chenchal Subraveti <chenchals@gmail.com>
2020-02-11 12:33:18 -0600

    Moved network and graphical analyses to scripts/graphNetwork folder

---
+++ scripts/paperFigs/graphNetwork/connectivityBasic.m
@@ -0,0 +1,8 @@
+% Using Olaf Sporns' Brain Connectivity Toolbox for analysis
+% See: https://sites.google.com/site/bctnet/
+% Complex network measures of brain connectivity: Uses and interpretations.
+%     Rubinov M, Sporns O (2010) NeuroImage 52:1059-69.
+%     https://doi.org/10.1016/j.neuroimage.2009.10.003
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
