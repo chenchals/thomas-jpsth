317e621e81eed26a213bd4f50b7b6e597fdd4b77
Chenchal Subraveti <chenchals@gmail.com>
2020-02-13 11:48:27 -0600

    Updated for verifying...

--- scripts/paperFigs/graphNetwork/connectionPattern.m
+++ scripts/paperFigs/graphNetwork/connectionPattern.m
@@ -32,13 +32,12 @@ rscTbl.signifRsc = zeros(size(rscTbl,1),1);
 idx = find(rscTbl.rhoRaw_150ms > 0 & rscTbl.pvalRaw_150ms <= 0.01);
 rscTbl.signifRsc(idx) = ones(numel(idx,1));
 idx = find(rscTbl.rhoRaw_150ms < 0 & rscTbl.pvalRaw_150ms <= 0.01);
-rscTbl.signifRsc(idx) = -1*ones(numel(idx,1));
+rscTbl.signifRsc(idx) = ones(numel(idx,1));
 
 %% count number of significant connections to build a heatmap matrix
 % 1. colNames =
 %           {'satCondition','rscSign','sefConnType','SEFvisMoveType','nPairs','nSignif',...
 %            'FEF_Grp1','FEF_Grp2','SC_Grp1','SC_Grp2',... % split nConnections as 1 + (n-1)
-%            'FEF_1','FEF_2','SC_1','SC_2'... % split nConnections n/2
 %            };
 %
 %           satCondition = {'Fast','Accurate'}
@@ -54,18 +53,6 @@ rscTbl.signifRsc(idx) = -1*ones(numel(idx,1));
 %         * 2 (sefConnType) 
 %       = 48 rows for each outcome epoch combination
 % 3. updating matrix-entry for a row: 
-%   Case1: Partial Connection count:  
-%      Example: current SEF unit is Vis then
-%      a. number of significant connections nc == 1
-%         i. if connection is to FEF then 
-%                update FEF_1 = FEF_1 + 0.5, FEF_2 = FEF_2 + 0.5
-%         ii. if connection is to SC then 
-%                update SC_1 = SC_1 + 0.5, SC_2 = SC_2 + 0.5
-%      b. number of significant connections nc > 1
-%         i. Split nc connections by area: nc_fef and nc_sc
-%         ii. update columns FEF_1 = FEF_1 + nc_fef/2, FEF_2 = FEF_2 + nc_fef/2
-%         ii. update columns SC_1 = SC_1 + nc_sc/2, SC_2 = SC_2 + nc_sc/2
-%   
 %   Case2: Whole connection count:
 %      Example: current SEF unit is Vis then
 %      a. number of significant connections nc == 1
@@ -84,12 +71,12 @@ rscTbl.signifRsc(idx) = -1*ones(numel(idx,1));
 %
 outcomes = {'Correct','ErrorChoice','ErrorTiming'};
 epochs = {'PostSaccade'};
-satConds = {'Fast','Accurate'};
-rscSigns = {'all','positive','negative'};
-sefConnTypes = {'single','multiple'};
-SEFVisMovType = {'Vis','VisMov','Mov','Other'};
+satConds = {'Accurate','Fast'};
+rscSigns = {'positive','negative','all'};
+sefConnTypes = {'multiple','single'};
+sefVisMovTypes = {'Vis','VisMov','Mov','Other'}';% row values
 colValsStr = {'satCondition','rscSign','sefConnType','SEFvisMoveType'};
-colValsNum = {'nPairs','nSignif','FEF_Grp1','FEF_Grp2','SC_Grp1','SC_Grp2','FEF_1','FEF_2','SC_1','SC_2'};
+colValsNum = {'nPairs','nSignif','FEF_Grp1','FEF_Grp2','SC_Grp1','SC_Grp2'};
 % Create an empty connection table 
 % for initiatizing tables in the loop below
 emptyConnTbl = table();
@@ -97,11 +84,11 @@ for rscSign = rscSigns
     for connType = sefConnTypes
         for sCond = satConds
             temp = table();
-            nr = numel(SEFVisMovType);
+            nr = numel(sefVisMovTypes);
             temp.satCondition = repmat(sCond,nr,1);
             temp.rscSign = repmat(rscSign,nr,1);
             temp.sefConnType = repmat(connType,nr,1);
-            temp.SEFvisMoveType = SEFVisMovType';
+            temp.sefVisMovType = sefVisMovTypes;
             for cn = colValsNum
                 temp.(cn{1}) = zeros(nr,1);
             end
@@ -127,79 +114,76 @@ for oc = 1:numel(outcomes)
            sefUnits = unique(rscs.X_unitNum);
            for u = 1:numel(sefUnits)
                unitNum = sefUnits(u);
-               currUnitPairs = fastAccuPairs(fastAccuPairs.X_unitNum == unitNum,:);
-               visMovType = char(unique(currUnitPairs.X_visMovType));
-                   % process for all Rsc do not take sign into account
-                   for rsig = 1:numel(rscSigns)
-                       rscSign = rscSigns{rsig};                       
-                       switch rscSign
-                           case 'positive'
-                               tempPairs = currUnitPairs(currUnitPairs.rscSign == 1,:);                              
-                           case 'negative'
-                               tempPairs = currUnitPairs(currUnitPairs.rscSign == -1,:);                                                       
-                           case 'all'
-                               tempPairs = currUnitPairs(abs(currUnitPairs.rscSign) == 1,:);
-                       end
+               unitPairs = fastAccuPairs(fastAccuPairs.X_unitNum == unitNum,:);
+               % since each unit has a mutually exclusive functional type
+               % no for loop for processing
+               visMovType = char(unique(unitPairs.X_visMovType));
+               % rsc signs +, -, all; use for loop...
+               for rsig = 1:numel(rscSigns)
+                   rscSign = rscSigns{rsig};
+                   switch rscSign
+                       case 'positive'
+                           signPairs = unitPairs(unitPairs.rscSign == 1,:);
+                       case 'negative'
+                           signPairs = unitPairs(unitPairs.rscSign == -1,:);
+                       case 'all'
+                           signPairs = unitPairs;
+                   end
+                   
+                   if ~isempty(signPairs)
+                       % all significant and non-significant pairs
+                       nPairs = size(signPairs,1);
+                       % output table row idx for  {'single','multiple'}
+                       outTblRoIdx = find(strcmp(outConn.satCondition,satCond) ...
+                           & strcmp(outConn.sefVisMovType,visMovType) ...
+                           & strcmp(outConn.rscSign,rscSign));
+                       % update values for signed pairs
+                       outConn.nPairs(outTblRoIdx) = outConn.nPairs(outTblRoIdx) + nPairs;
                        
-                       if isempty(tempPairs)
-                           continue;
-                       end
-                       % assume there is only 1 connection for this unit
-                       nPairs = size(tempPairs,1);
-                       % retrieve all row index of output table {single'
-                       % and 'multiple'
-                       idx = find(ismember(outConn.satCondition,satCond) ...
-                                  & ismember(outConn.SEFvisMoveType,visMovType) ...
-                                  & ismember(outConn.rscSign,rscSign));
-                       % update values
-                       outConn.nPairs(idx) = outConn.nPairs(idx) + nPairs;
-                         
-                       % Prune to significant connections
-                       tempPairs(tempPairs.signifRsc == 0,:) = [];                       
-                       if isempty(tempPairs)
-                           continue; % no significant pairs
-                       end
-                       nSignif = size(tempPairs,1);
-                       sefConnType = 'single';
-                       if nSignif > 1
-                           sefConnType = 'multiple';
-                       end
-                       % update output table
-                       % retrieve row index of output table
-                       idx = find(ismember(outConn.satCondition,satCond) ...
-                                  & ismember(outConn.SEFvisMoveType,visMovType) ...
-                                  & ismember(outConn.rscSign,rscSign)...
-                                  & ismember(outConn.sefConnType,sefConnType));
-                       
-                       % update nSignif values in the retrieved row
-                       outConn.nSignif(idx) = outConn.nSignif(idx) + nSignif;
-                       % update FEF_1 and FEF_2 values
-                       nc = numel(find(ismember(tempPairs.Y_area,'FEF')));
-                       % case1:
-                       outConn.FEF_1(idx) = outConn.FEF_1(idx) + nc/2;
-                       outConn.FEF_2(idx) = outConn.FEF_2(idx) + nc/2;
-                       if nc > 0
-                           % case2:
-                           outConn.FEF_Grp1(idx) = outConn.FEF_Grp1(idx) + 1;
-                           nc = nc - 1;
-                           nc = (abs(nc) + nc)/2; % nc is 0 or positive
-                           outConn.FEF_Grp2(idx) = outConn.FEF_Grp2(idx) + nc;
-                       end
-                       % update SC_1 and SC_2 values
-                       % case1:
-                       nc = numel(find(ismember(tempPairs.Y_area,'SC')));
-                       outConn.SC_1(idx) = outConn.SC_1(idx) + nc/2;
-                       outConn.SC_2(idx) = outConn.SC_2(idx) + nc/2;
-                       % case2:
-                       if nc > 0
-                           outConn.SC_Grp1(idx) = outConn.SC_Grp1(idx) + 1;
-                           nc = nc - 1;
-                           nc = (abs(nc) + nc)/2; % nc is 0 or positive
-                           outConn.SC_Grp2(idx) = outConn.SC_Grp2(idx) + nc;
-                       end
-                       
-                  end
+                       % process only significant pairs
+                       signifPairs = signPairs(signPairs.signifRsc == 1,:);
+                       nSignif = size(signifPairs,1);
  
+                       if nSignif > 0
+                           if nSignif > 1
+                               sefConnType = 'multiple';
+                           else
+                               sefConnType = 'single';
+                           end
+                           % update output table
+                           % retrieve row index of output table
+                           outTblRoIdx = find(strcmp(outConn.satCondition,satCond) ...
+                               & strcmp(outConn.sefVisMovType,visMovType) ...
+                               & strcmp(outConn.rscSign,rscSign)...
+                               & strcmp(outConn.sefConnType,sefConnType));
+                           
+                           % update nSignif values in the retrieved row
+                           outConn.nSignif(outTblRoIdx) = outConn.nSignif(outTblRoIdx) + nSignif;
+                           % update FEF_1 and FEF_2 values
+                           nc = sum(strcmp(signifPairs.Y_area,'FEF'));
+                           if nc > 0
+                               % case2:
+                               outConn.FEF_Grp1(outTblRoIdx) = outConn.FEF_Grp1(outTblRoIdx) + 1;
+                               if (nc > 1)
+                                   outConn.FEF_Grp2(outTblRoIdx) = outConn.FEF_Grp2(outTblRoIdx) + nc - 1;
+                               end
+                           end
+                           % update SC_1 and SC_2 values
+                           nc = sum(strcmp(signifPairs.Y_area,'SC'));
+                           if nc > 0
+                               % case2:
+                               outConn.SC_Grp1(outTblRoIdx) = outConn.SC_Grp1(outTblRoIdx) + 1;
+                               if (nc > 1)
+                                   outConn.SC_Grp2(outTblRoIdx) = outConn.SC_Grp2(outTblRoIdx) + nc - 1;
+                               end
+                           end
+                           %if ~isempty(signifPairs)
+                       end
+                       % ~isempty(signPairs)
+                   end
+                   % next rscSign
+               end
+               
                % next unit num
            end
            outConnMat.(outFieldname) = outConn;
@@ -213,7 +197,7 @@ end
 % redifine stuff for creating heat maps
 fns = fieldnames(outConnMat); % CorrectPostSaccade, ErrorChoicePostSaccade, ErrorTimingPostSaccade
 sefConnTypes = {'single','multiple'};
-satCons = {'Fast','Accurate'};
+satConds = {'Fast','Accurate'};
 rscSigns = {'all','positive','negative'};
 heatmapXCols = {'FEF_Grp1','FEF_Grp2','SC_Grp1','SC_Grp2'};
 heatmapYCols = {'SEFvisMoveType'};
@@ -230,7 +214,7 @@ for oe =1 :numel(fns)
     tempVals = [tempVals;outConnMat.(fns{oe}){:,heatmapXCols}];
 end
 tempVals = tempVals(:);
-cLims = [min(tempVals),max(tempVals)];
+cLims = [1,max(tempVals)];
 
 for oe = 1:numel(fns)
     outcomeEpoch = fns{oe};
@@ -253,18 +237,22 @@ for oe = 1:numel(fns)
             end
         end
     end  
-    saveFigPdf([outcomeEpoch '.pdf']);
-    delete(f)
+    %saveFigPdf([outcomeEpoch '.pdf']);
+    %delete(f)
 end
 
 
 %%
 function [h_heatmap] = fx_plotHeatmap(h_axis,dat,heatmapXCols,cLims) 
     cData = dat{:,heatmapXCols};
+    cData(cData == 0) = NaN;
     xLabel = regexprep(heatmapXCols','_Grp','');
-    yLabel = dat.SEFvisMoveType;
+    yLabel = dat.sefVisMovType;
     axes(h_axis)
     h_heatmap = heatmap(xLabel,yLabel,cData,'ColorLimits',cLims);
     colormap('cool')
+    h_heatmap.MissingDataColor = [0.95 0.95 0.95];
+    h_heatmap.MissingDataLabel = 'No Conn.';
+    h_heatmap.ColorbarVisible = 'off';
 end
 
