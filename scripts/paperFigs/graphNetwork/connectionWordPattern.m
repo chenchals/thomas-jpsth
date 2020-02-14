b9e42f4dc4858a1b3c548769acb36f25147152fc
Chenchal Subraveti <chenchals@gmail.com>
2020-02-14 16:53:41 -0600

    updated for pattern tables

--- scripts/paperFigs/graphNetwork/connectionWordPattern.m
+++ scripts/paperFigs/graphNetwork/connectionWordPattern.m
@@ -48,122 +48,241 @@ sefAll = unique(rscTbl.X_unitNum);
 fefAll = unique(rscTbl.Y_unitNum(ismember(rscTbl.Y_area,'FEF')));
 scAll = unique(rscTbl.Y_unitNum(ismember(rscTbl.Y_area,'SC')));
 
-
 %% build the word pattern table for significant connections
 sigRscTbl = rscTbl(rscTbl.signifRsc == 1,:);
 sefSig = unique(sigRscTbl.X_unitNum);
 fefSig = unique(sigRscTbl.Y_unitNum(ismember(sigRscTbl.Y_area,'FEF')));
 scSig = unique(sigRscTbl.Y_unitNum(ismember(sigRscTbl.Y_area,'SC')));
+connPatternTbl = getConnWordPattern(sigRscTbl);
+% uniqDecToBinPattern = unique(wordPattern(:,{'connWord6bitDec', 'connWord4bitDec','connWord6bit','connWord4bit'}));
+funcWordPattern = getFuncWordPattern(connPatternTbl);
 
-% filter table for outcomeEpoch 
-epochs = {'PostSaccade'};
-outcomes = {'Correct','ErrorChoice','ErrorTiming'};
-satConds = {'Fast','Accurate','Fast_Accurate'};
+%% permute functional type labels on the connPatternTbl to simulate random functional type associations
+sefUnitType = unique(sigRscTbl(:,{'X_unitNum','X_visMovType'}));
+sefUnits = sefUnitType.X_unitNum;
+nPerms = 10;
+randFuncWordPatternPerms = cell(nPerms,1);
+for np = 1:nPerms
+    fprintf('premutation #%d\n',np);
+    % permute unit labels
+    randSefType = sefUnitType.X_visMovType(randperm(numel(sefUnits)));
+    fn = ['rand_' num2str(np,'%d')];
+    sefUnitType.(fn) = randSefType;
+    % assign this random func type to table variable
+    rndRscTbl = sigRscTbl;
+    for u = 1:numel(sefUnits)
+        uNum = sefUnits(u);
+        nType = sefUnitType.(fn)(sefUnitType.X_unitNum == uNum);
+        idx = rndRscTbl.X_unitNum == uNum;
+        rndRscTbl.X_visMovType(idx) = repmat(nType,sum(idx),1);
+    end
+    Z = getConnWordPattern(rndRscTbl);
+    randFuncWordPatternPerms{np} = getFuncWordPattern(Z);
+end
+% Create mean_GroupCount for the randFuncWordPattern 
+randFuncWordPatternAll = table();
+for ii = 1:size(randFuncWordPatternPerms,1)
+    randFuncWordPatternAll = [randFuncWordPatternAll;randFuncWordPatternPerms{1}];
+end
+useVars = {'outcome','epoch','satCondition','sefVisMovType','connWord6bitDec','connWord4bitDec','connWord6bit','connWord4bit','GroupCount'};
 
-outConnWordPattern = table();
-for ep = 1:numel(epochs)
-    epoch = epochs{ep};
-    for oc = 1:numel(outcomes)
-        outcome = outcomes{oc};
-        rscs = sigRscTbl(ismember(sigRscTbl.epoch,epoch) & ismember(sigRscTbl.outcome,outcome),:);
-        for sc = 1:numel(satConds)
-            satCond = satConds{sc};
-            if strcmp(satCond,'Fast_Accurate')
-                dat = rscs;
-            else
-                dat = rscs(ismember(rscs.satCondition,satCond),:);
-            end
-            sefUnitNums = unique(dat.X_unitNum);
-            for u = 1:numel(sefUnitNums)
-                unitNum = sefUnitNums(u);
-                unitPairs = dat(dat.X_unitNum == unitNum,:);
-                plusIdx = unitPairs.rscSign == 1;
-                minusIdx = unitPairs.rscSign == -1;
-                % Count FEF plus/minus pairs
-                fefNconnPlus = sum(ismember(unitPairs.Y_area,'FEF') & plusIdx);
-                fefNconnMinus = sum(ismember(unitPairs.Y_area,'FEF') & minusIdx);
-                % Count SC plus/minus pairs
-                scNconnPlus = sum(ismember(unitPairs.Y_area,'SC') & plusIdx);
-                scNconnMinus = sum(ismember(unitPairs.Y_area,'SC') & minusIdx);
-                % build a table row of output
-                wTbl = table();
-                wTbl.outcome{1} = outcome;
-                wTbl.epoch{1} = epoch;
-                wTbl.satCondition{1} = satCond;
-                wTbl.sefVisMovType = unique(unitPairs.X_visMovType);
-                wTbl.sefUnitNum = unitNum;
-                wTbl.nSignifPairs = size(unitPairs,1);
-                wTbl.nSignifPlusRsc = sum(plusIdx);
-                wTbl.nSignifMinusRsc = sum(minusIdx);
-                % parse the pairs for this unit into a bit pattern word
-                % initialize bit pattern
-                wTbl.FEFconnected = 0; 
-                wTbl.SCconnected = 0; 
-                wTbl.FEFplus1 = 0;
-                wTbl.FEFminus1 = 0;
-                wTbl.SCplus1 = 0;
-                wTbl.SCminus1 = 0;
-                % for FEF pairs
-                if fefNconnPlus > 0
-                    wTbl.FEFconnected = 1;
-                    wTbl.FEFplus1 = 1;
-                end
-                if fefNconnMinus > 0
-                    wTbl.FEFconnected = 1;
-                    wTbl.FEFminus1 = 1;
-                end
-                % for SC pairs
-                if scNconnPlus > 0
-                    wTbl.SCconnected = 1;
-                    wTbl.SCplus1 = 1;
-                end
-                if scNconnMinus > 0
-                    wTbl.SCconnected = 1;
-                    wTbl.SCminus1 = 1;
-                end
-                outConnWordPattern = [outConnWordPattern; wTbl]; %#ok<*AGROW>
-                % done for this unit
-            end
-        end
-    end    
+randFuncWordPattern = grpstats(randFuncWordPatternAll(:,useVars),useVars(1:end-1),'mean');
+randFuncWordPattern.Properties.RowNames = {};
+colNames = randFuncWordPattern.Properties.VariableNames;
+colNames = regexprep(colNames,{'^GroupCount$','^mean_GroupCount$'},{'nPerms','GroupCount'});
+randFuncWordPattern.Properties.VariableNames = colNames;
+
+%% now we have 
+% funcWordPattern and randFuncWordPattern also sefUnitType for what random
+% combinations were used for randFuncWordPattern
+% remove all vars except these
+clearvars -except funcWordPattern randFuncWordPattern sefUnitType
+% create all possible patterns There will be 15 unique patters
+% 
+uniqPatternTbl = table();
+for ii = 1:15
+    pat = table();
+    p = dec2bin(ii,4);
+    pat.patIdx = ii;
+    pat.connWord4bitDec = ii;
+    pat.connWord4bit = {p};
+    pat.FEFplus1 = p(1);
+    pat.FEFminus1 = p(2);
+    pat.SCplus1 = p(3);
+    pat.SCminus1 = p(4);
+    uniqPatternTbl =[uniqPatternTbl;pat];
 end
 
-% add column for 6 bit word
-nRows = size(outConnWordPattern,1);
-colsFor6bitWord = {'FEFconnected','SCconnected','FEFplus1','FEFminus1','SCplus1','SCminus1'};
-colsFor4bitWord = {'FEFplus1','FEFminus1','SCplus1','SCminus1'};
-outConnWordPattern.connWord6bit = arrayfun(@(x) sprintf('%d',outConnWordPattern{x,colsFor6bitWord}),(1:nRows)','UniformOutput',false);
-outConnWordPattern.connWord4bit = arrayfun(@(x) sprintf('%d',outConnWordPattern{x,colsFor4bitWord}),(1:nRows)','UniformOutput',false);
-% convert bit pattern to decimal
-outConnWordPattern.connWord6bitDec = arrayfun(@(x) bin2dec(x{1}),outConnWordPattern.connWord6bit);
-outConnWordPattern.connWord4bitDec = arrayfun(@(x) bin2dec(x{1}),outConnWordPattern.connWord4bit);
 
-connWordDecToBinPattern = unique(outConnWordPattern(:,{'connWord6bitDec', 'connWord4bitDec','connWord6bit','connWord4bit'}));
+patColNames = strcat('pat_',num2str(uniqPatternTbl.connWord4bitDec,'%02d_'),uniqPatternTbl.connWord4bit)';
+featureColNames = {'outcome','epoch','satCondition','sefVisMovType'};
+permuteColNames = {'dataSrc','nPrems'};
+useCols = [permuteColNames featureColNames patColNames];
+patternSummTbl = table();
 
-% Count different patterns by SEF functional types
-epochs = {'PostSaccade'};
-outcomes = {'Correct','ErrorChoice','ErrorTiming'};
-satConds = {'Fast','Accurate','Fast_Accurate'};
-connPatternTbl = outConnWordPattern;
-useVars = {'satCondition','sefVisMovType','connWord6bitDec','connWord4bitDec'};
-grpVars = useVars; %same order
-funcWordWattern = struct();
-for oc = 1:numel(outcomes)
-    outcome = outcomes{oc};
-    idxOutcome = ismember(connPatternTbl.outcome,outcome);
-    for ep = 1:numel(epochs)
-        epoch = epochs{ep};
-        idxEpoch = ismember(connPatternTbl.epoch,epoch);
-        for sc = 1:numel(satConds)
-            satCond = satConds{sc};
-            fn = [outcome epoch satCond];
-            idx =  ismember(connPatternTbl.satCondition,satCond) & idxOutcome & idxEpoch;
-            temp = connPatternTbl(idx,:);
-            res = grpstats(temp(:,useVars),grpVars,{'counts'});
-            funcWordWattern.(fn) = res;
-        end        
+tbls = {funcWordPattern,randFuncWordPattern};
+for ii = 1:numel(tbls)
+    dataSrc = 'actual';
+    nPerms = 0;
+    dat = tbls{ii};
+    if ii == 2
+        dataSrc = 'randPerm';
+        nPerms = unique(dat.nPerms);
+    end
+    % build table row for each entry in the dat table
+    nRows = size(dat,1);
+    for nr = 1:nRows
+        datRo = dat(nr,:);
+        outcome = datRo.outcome;
+        epoch = datRo.epoch;
+        satCondition = datRo.satCondition;
+        sefVisMovType = datRo.sefVisMovType;
+        groupCount = datRo.GroupCount;
+        pattern4colName = char(strcat('pat_', num2str(datRo.connWord4bitDec,'%02d_'), datRo.connWord4bit));
+        
+        % find idx into table to aggregate across patterns
+        idx = 0;
+        if ~isempty(patternSummTbl)
+        idx = find(ismember(patternSummTbl.dataSrc,dataSrc) & patternSummTbl.nPerms == nPerms...
+            & ismember(patternSummTbl.outcome,outcome) & ismember(patternSummTbl.epoch,epoch)...
+            & ismember(patternSummTbl.sefVisMovType,sefVisMovType));
+        end
+        if idx > 0
+            patternSummTbl.(pattern4colName)(idx) = groupCount;
+        else
+            p = table();
+            p.dataSrc{1} = dataSrc;
+            p.nPerms = nPerms;
+            p.outcome = outcome;
+            p.epoch = epoch;
+            p.satCondition = satCondition;
+            p.sefVisMovType = sefVisMovType;
+            p = [p array2table(repmat(0,1,numel(patColNames)),'VariableNames',patColNames)];
+            p.(pattern4colName) = groupCount;
+            patternSummTbl = [patternSummTbl; p];
+        end
     end
 end
+
+% sum total count across rows
+patternSummTbl.nAllPatterns = sum(patternSummTbl{:,patColNames},2);
+
+%clearvars -except funcWordPattern randFuncWordPattern sefUnitType uniqPatternTbl patternSummTbl
+
+
+
+
+%% Get word pattern tables for original input Rsc table
+function [connWordPattern] = getConnWordPattern(inRscTbl)
+    % filter table for outcomeEpoch
+    epochs = {'PostSaccade'};
+    outcomes = {'Correct','ErrorChoice','ErrorTiming'};
+    satConds = {'Fast','Accurate'};
+
+    connWordPattern = table();
+    for ep = 1:numel(epochs)
+        epoch = epochs{ep};
+        for oc = 1:numel(outcomes)
+            outcome = outcomes{oc};
+            rscs = inRscTbl(ismember(inRscTbl.epoch,epoch) & ismember(inRscTbl.outcome,outcome),:);
+            for sc = 1:numel(satConds)
+                satCond = satConds{sc};
+                dat = rscs(ismember(rscs.satCondition,satCond),:);
+                sefUnitNums = unique(dat.X_unitNum);
+                for u = 1:numel(sefUnitNums)
+                    unitNum = sefUnitNums(u);
+                    unitPairs = dat(dat.X_unitNum == unitNum,:);
+                    plusIdx = unitPairs.rscSign == 1;
+                    minusIdx = unitPairs.rscSign == -1;
+                    % Count FEF plus/minus pairs
+                    fefNconnPlus = sum(ismember(unitPairs.Y_area,'FEF') & plusIdx);
+                    fefNconnMinus = sum(ismember(unitPairs.Y_area,'FEF') & minusIdx);
+                    % Count SC plus/minus pairs
+                    scNconnPlus = sum(ismember(unitPairs.Y_area,'SC') & plusIdx);
+                    scNconnMinus = sum(ismember(unitPairs.Y_area,'SC') & minusIdx);
+                    % build a table row of output
+                    wTbl = table();
+                    wTbl.outcome{1} = outcome;
+                    wTbl.epoch{1} = epoch;
+                    wTbl.satCondition{1} = satCond;
+                    wTbl.sefVisMovType = unique(unitPairs.X_visMovType);
+                    wTbl.sefUnitNum = unitNum;
+                    wTbl.nSignifPairs = size(unitPairs,1);
+                    wTbl.nSignifPlusRsc = sum(plusIdx);
+                    wTbl.nSignifMinusRsc = sum(minusIdx);
+                    % parse the pairs for this unit into a bit pattern word
+                    % initialize bit pattern
+                    wTbl.FEFconnected = 0;
+                    wTbl.SCconnected = 0;
+                    wTbl.FEFplus1 = 0;
+                    wTbl.FEFminus1 = 0;
+                    wTbl.SCplus1 = 0;
+                    wTbl.SCminus1 = 0;
+                    % for FEF pairs
+                    if fefNconnPlus > 0
+                        wTbl.FEFconnected = 1;
+                        wTbl.FEFplus1 = 1;
+                    end
+                    if fefNconnMinus > 0
+                        wTbl.FEFconnected = 1;
+                        wTbl.FEFminus1 = 1;
+                    end
+                    % for SC pairs
+                    if scNconnPlus > 0
+                        wTbl.SCconnected = 1;
+                        wTbl.SCplus1 = 1;
+                    end
+                    if scNconnMinus > 0
+                        wTbl.SCconnected = 1;
+                        wTbl.SCminus1 = 1;
+                    end
+                    connWordPattern = [connWordPattern; wTbl]; %#ok<*AGROW>
+                    % done for this unit
+                end
+            end
+        end
+    end
+
+    % add column for 6 bit word
+    nRows = size(connWordPattern,1);
+    colsFor6bitWord = {'FEFconnected','SCconnected','FEFplus1','FEFminus1','SCplus1','SCminus1'};
+    colsFor4bitWord = {'FEFplus1','FEFminus1','SCplus1','SCminus1'};
+    connWordPattern.connWord6bit = arrayfun(@(x) sprintf('%d',connWordPattern{x,colsFor6bitWord}),(1:nRows)','UniformOutput',false);
+    connWordPattern.connWord4bit = arrayfun(@(x) sprintf('%d',connWordPattern{x,colsFor4bitWord}),(1:nRows)','UniformOutput',false);
+    % convert bit pattern to decimal
+    connWordPattern.connWord6bitDec = arrayfun(@(x) bin2dec(x{1}),connWordPattern.connWord6bit);
+    connWordPattern.connWord4bitDec = arrayfun(@(x) bin2dec(x{1}),connWordPattern.connWord4bit);
+
+end
+
+
+
+%% Count different patterns by SEF functional types
+function [funcWordTbl] = getFuncWordPattern(connPatternTbl)
+    epochs = {'PostSaccade'};
+    outcomes = {'Correct','ErrorChoice','ErrorTiming'};
+    satConds = {'Fast','Accurate'};
+    useVars = {'outcome','epoch','satCondition','sefVisMovType','connWord6bitDec','connWord4bitDec','connWord6bit','connWord4bit'};
+    grpVars = useVars; %same order
+    funcWordTbl = table();
+    for oc = 1:numel(outcomes)
+        outcome = outcomes{oc};
+        idxOutcome = ismember(connPatternTbl.outcome,outcome);
+        for ep = 1:numel(epochs)
+            epoch = epochs{ep};
+            idxEpoch = ismember(connPatternTbl.epoch,epoch);
+            for sc = 1:numel(satConds)
+                satCond = satConds{sc};
+                idx =  ismember(connPatternTbl.satCondition,satCond) & idxOutcome & idxEpoch;
+                temp = connPatternTbl(idx,:);
+                res = grpstats(temp(:,useVars),grpVars,{'counts'});
+                res.Properties.RowNames = {};
+                funcWordTbl = [funcWordTbl;res];
+            end
+        end
+    end
+end
+
 
 
               
